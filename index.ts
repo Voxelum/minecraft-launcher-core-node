49192d52055beed397ad0b362d8a661ef9b440be
ci010 <cijhn@hotmail.com>
2017-07-31 14:56:51 +0800

    fix api expo

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { GameProfile, UserType, AuthResponse, AuthService } from './src/auth'
 import {
     GameType, WorldInfo, Language, GameSetting,
-    ServerInfo, ServerStatus, ResourceMode, ResourcePack,
+    ServerInfo, ServerStatus, ResourceMode, ResourcePack, ServerStatusFrame,
     ModContainer, ModIndentity, ForgeMetaData, LiteModMetaData
 } from './src/game'
 import { TextComponent, TextFormatting, Style } from './src/text'
@@ -21,7 +21,8 @@ export {
     GameProfile, UserType,
     Version, VersionMeta, VersionMetaList,
     TextComponent, TextFormatting, Style,
-    GameType, WorldInfo, Language, ServerInfo, ServerStatus,
+    GameType, WorldInfo, Language, ServerInfo, 
+    ServerStatus, ServerStatusFrame,
     ResourceMode, ResourcePack, ModContainer,
     ModIndentity, ForgeMetaData, LiteModMetaData,
     MinecraftFolder, ResourceLocation,
