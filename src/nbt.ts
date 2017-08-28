383ae57ef948cf289ec1789c2f5b01331d712d07
ci010 <cijhn@hotmail.com>
2017-08-28 15:16:35 +0800

    refactor nbt

--- src/nbt.ts
+++ src/nbt.ts
@@ -3,25 +3,83 @@ import * as ByteBuffer from 'bytebuffer'
 
 //not finished yet...
 export namespace NBT {
+    interface IO {
+        read(buf: ByteBuffer, find: (shape: string) => string | undefined): { type: any, value: any };
+        write(buf: ByteBuffer, value: any, scope: any, find: (id: string) => any): void;
+    }
     export enum Type {
         End = 0, Byte = 1, Short = 2, Int = 3, Long = 4, Float = 5, Double = 6,
         ByteArray = 7, String = 8, List = 9, Compound = 10, IntArray = 11
     }
 
-    let visitors: Visitor[] = [
-        { read(buf) { }, write(buf, v) { } }, //end
-        { read: (buf) => buf.readByte(), write(buf, v) { buf.writeByte(v) } }, //byte
-        { read: (buf) => buf.readShort(), write(buf, v) { buf.writeShort(v) } }, //short
-        { read: (buf) => buf.readInt(), write(buf, v) { buf.writeInt(v) } }, //int
-        { read: (buf) => buf.readLong(), write(buf, v) { buf.writeInt64(v) } }, //long
-        { read: (buf) => buf.readFloat(), write(buf, v) { buf.writeFloat(v) } }, //float
-        { read: (buf) => buf.readDouble(), write(buf, v) { buf.writeDouble(v) } }, //double
+    export function parse(fileData: Buffer, compressed: boolean = false, find: (shape: string) => string | undefined = () => undefined): { value: any, type: any | string } {
+        if (compressed) {
+            let zip = gzip.gunzipSync(fileData);
+            let bytebuffer = ByteBuffer.wrap(zip);
+            return readRootTag(bytebuffer, find)
+        }
+        else {
+            let bytebuffer = ByteBuffer.wrap(fileData);
+            return readRootTag(bytebuffer, find)
+        }
+    }
+
+    export function serializer() {
+        return new Serializer()
+    }
+
+    export class Serializer {
+        private registry: { [id: string]: any } = {};
+        private reversedRegistry: { [shape: string]: any } = {};
+        register(type: string, schema: any): this {
+            this.registry[type] = schema;
+            this.reversedRegistry[JSON.stringify(schema)] = type;
+            return this;
+        }
+        serialize(object: object, type: string, compressed: boolean = false) {
+            let template;
+            const schema = this.registry[type]
+            if (!schema) throw `Unknown type [${schema}]`
+
+            const buffer = new ByteBuffer();
+            buffer.writeByte(Type.Compound);
+            writeUTF8(buffer, '');
+            visitors[Type.Compound].write(buffer, object, schema, (id) => this.registry[id])
+
+            if (compressed) {
+                return gzip.gzipSync(Buffer.from(buffer.flip().toArrayBuffer()));
+            } else {
+                return Buffer.from(buffer.flip().toArrayBuffer());
+            }
+        }
+        deserialize(buf: Buffer, compressed: boolean = false): { value: any, type: any | string } {
+            return parse(buf, compressed, (shape) => this.reversedRegistry[shape]);
+        }
+    }
+
+    function readRootTag(buffer: ByteBuffer, find: (shape: string) => string | undefined) {
+        let rootType = buffer.readByte();
+        if (rootType == 0) throw new Error('NBTEnd');
+        if (rootType != 10) throw new Error("Root tag must be a named compound tag.");
+        readUTF8(buffer); //I think this is the nameProperty of the file...
+        return visitors[Type.Compound].read(buffer, find);
+    }
+
+    function val(type: any, value: any) { return { type, value } }
+    const visitors: IO[] = [
+        { read: (buf) => val(NBT.Type.End, undefined), write(buf, v) { } }, //end
+        { read: (buf) => val(NBT.Type.Byte, buf.readByte()), write(buf, v) { buf.writeByte(v) } }, //byte
+        { read: (buf) => val(NBT.Type.Short, buf.readShort()), write(buf, v) { buf.writeShort(v) } }, //short
+        { read: (buf) => val(NBT.Type.Int, buf.readInt()), write(buf, v) { buf.writeInt(v) } }, //int
+        { read: (buf) => val(NBT.Type.Long, buf.readLong()), write(buf, v) { buf.writeInt64(v) } }, //long
+        { read: (buf) => val(NBT.Type.Float, buf.readFloat()), write(buf, v) { buf.writeFloat(v) } }, //float
+        { read: (buf) => val(NBT.Type.Double, buf.readDouble()), write(buf, v) { buf.writeDouble(v) } }, //double
         { //byte array
             read(buf) {
                 let len = buf.readInt();
                 let arr: number[] = new Array(len);
                 for (let i = 0; i < len; i++) arr[i] = buf.readByte();
-                return arr;
+                return val(NBT.Type.ByteArray, arr);
             },
             write(buf, arr) {
                 buf.writeInt(arr.length)
@@ -29,99 +87,95 @@ export namespace NBT {
             }
         },
         { //string
-            read(buf) { return readUTF8(buf); },
+            read(buf) { return val(NBT.Type.String, readUTF8(buf)); },
             write(buf, v) { writeUTF8(buf, v) }
         },
         { //list
-            read(buf, schemaScope) {
-                let id = buf.readByte();
+            read(buf, find) {
+                let listType = buf.readByte();
                 let len = buf.readInt();
                 let arr = new Array(len);
-                let visitor = visitors[id];
-                if (id == Type.Compound) {
-                    for (let i = 0; i < len; i++) {
-                        let newScope: object = {};
-                        arr[i] = visitor.read(buf, newScope);
-                        schemaScope.push(newScope)
-                    }
+                let visitor = visitors[listType];
+                let scope: any[] = []
+                for (let i = 0; i < len; i++) {
+                    let newScope: object = {};
+                    const { type, value } = visitor.read(buf, find);
+                    arr[i] = value;
+                    scope.push(type)
                 }
-                else for (let i = 0; i < len; i++) arr[i] = visitor.read(buf);
-                return arr;
+                if (scope.every(val => val === scope[0]))
+                    scope = [scope[0]]
+                return val(scope, arr);
             },
-            write(buf, value, context) {
-                const { scope, find } = context
+            write(buf, value, scope, find) {
                 const type = scope[0]
-                if (typeof (type) === 'number') {
+                if (typeof type === 'number') {
+                    buf.writeByte(type);
+                    buf.writeInt(value.length);
                     let writer = visitors[type]
-                    for (let v of value)
-                        writer.write(buf, v, { scope: type, find })
+                    for (let v of value) writer.write(buf, v, type, find)
                 }
-                else if (typeof (type) === 'string') {
-                    let found = find(type)
-                    if (!found) throw new Error(`Cannot find type ${type}!`)
+                else if (typeof type === 'string') {
+                    let customScope = find(type)
+                    if (!customScope) throw `Unknown custom type [${type}]`
                     buf.writeByte(NBT.Type.Compound);
                     buf.writeInt(value.length);
                     let writer = visitors[NBT.Type.Compound];
-                    for (let v of value)
-                        writer.write(buf, v, { scope: found, find })
+                    for (let v of value) writer.write(buf, v, customScope, find)
                 }
-                else if (typeof (type) === 'object') {
+                else if (typeof type === 'object') {
                     let writer = visitors[NBT.Type.Compound];
-                    for (let v of value)
-                        writer.write(buf, v, { scope: type, find })
+                    for (let v of value) writer.write(buf, v, type, find)
                 }
                 else throw new Error("WTH")
             }
         },
         {//tag compound
-            read(buf, scope) {
-                let object: any = {};
-                for (let type = 0; (type = buf.readByte()) != 0;) {
-                    let name = visitors[Type.String].read(buf);
-                    let visitor = visitors[type]
-                    if (!visitor)
-                        throw "No such tag id " + type;
-                    if (type == Type.Compound) {
-                        let newScope = {};
-                        scope[name] = newScope;
-                        object[name] = visitor.read(buf, newScope)
-                    } else if (type == Type.List) {
-                        let newScope: any[] | number = [];
-                        object[name] = visitor.read(buf, newScope)
-                        if (newScope.length == 1)
-                            if (typeof (newScope[0]) === 'number')
-                                newScope = newScope[0]
-                        scope[name] = newScope;
-                    } else {
-                        scope[name] = type;
-                        object[name] = visitor.read(buf)
-                    }
+            read(buf, find) {
+                const object: any = {};
+                let scope: any = {};
+                for (let tagType = 0; (tagType = buf.readByte()) != 0;) {
+                    const name = readUTF8(buf);
+                    const visitor = visitors[tagType];
+                    if (!visitor) throw "No such tag id " + tagType;
+                    const { type, value } = visitor.read(buf, find);
+                    object[name] = value;
+                    scope[name] = type;
                 }
-                return object;
+                const shape = JSON.stringify(scope);
+                const id = find(shape);
+                if (id) scope = id;
+                return val(scope, object);
             },
-            write(buf, obj, context) {
-                const { scope, find } = context
-                for (let key in obj) {
-                    visitors[Type.String].write(buf, key, context)
-                    let value = obj[key]
-                    const scopeValue = scope[key]
-                    const $type = typeof scopeValue;
-                    if ($type === 'number') {
-                        let writer = visitors[scopeValue]
-                        if (!writer) throw "Unknown type " + scopeValue;
-                        writer.write(buf, value, context)
-                    } else if ($type === 'string') {
-                        let writer = visitors[NBT.Type.Compound]
-                        //support custom type
-                        writer.write(buf, value, { scope: find(scopeValue), find })
-                    } else if (scopeValue instanceof Array) {
-                        let writer = visitors[NBT.Type.List]
-                        writer.write(buf, value, { scope: scopeValue, find })
-                    } else if ($type === 'object') {
-                        let writer = visitors[NBT.Type.Compound]
-                        writer.write(buf, value, { scope: scopeValue, find })
-                    } else throw new Error("WTF")
+            write(buf, object, scope, find) {
+                for (const key in object) {
+                    let value = object[key]
+                    const type = scope[key]
+                    let nextScope = undefined;
+                    let nextType = type;
+                    let writer;
+
+                    if (typeof type === 'number') {
+                        nextType = type
+                    } else if (type instanceof Array) {
+                        nextScope = type;
+                        nextType = NBT.Type.List
+                    } else if (typeof type === 'string') {
+                        nextType = NBT.Type.Compound;
+                        nextScope = find(type);  //support custom type
+                        if (!nextScope) throw `Unknown custom type [${type}]`
+                    } else if (typeof type === 'object') {
+                        nextType = NBT.Type.Compound
+                        nextScope = type;
+                    } else throw `Invalid type [${type}]`
+
+                    writer = visitors[nextType];
+                    buf.writeByte(nextType)
+                    writeUTF8(buf, key)
+                    if (!writer) throw "Unknown type " + type;
+                    writer.write(buf, value, nextScope, find)
                 }
+                buf.writeByte(0)
             }
         },
         { //int array
@@ -129,7 +183,7 @@ export namespace NBT {
                 let len = buf.readInt();
                 let arr: number[] = new Array(len);
                 for (let i = 0; i < len; i++) arr[i] = buf.readInt();
-                return arr;
+                return val(NBT.Type.IntArray, arr);
             },
             write(buf, v) {
                 buf.writeInt(v.length)
@@ -138,145 +192,9 @@ export namespace NBT {
         }
     ];
 
-    interface Visitor {
-        read(buf: ByteBuffer, schemaObject?: any): any;
-        write(buf: ByteBuffer, value: any, context: { scope?: any, find: (id: string) => any }): void;
-    }
-
-    export function parse(fileData: Buffer, compressed: boolean = false): { root: any, schema: any } {
-        if (compressed) {
-            let zip = gzip.gunzipSync(fileData);
-            let bytebuffer = ByteBuffer.wrap(zip);
-            return readRootTag(bytebuffer)
-        }
-        else {
-            let bytebuffer = ByteBuffer.wrap(fileData);
-            return readRootTag(bytebuffer)
-        }
-    }
-
-    function readRootTag(buffer: ByteBuffer): { root: any, schema: any } {
-        let rootType = buffer.readByte();
-        if (rootType == 0) throw new Error('NBTEnd');
-        if (rootType != 10) throw new Error("Root tag must be a named compound tag.");
-        visitors[Type.String].read(buffer); //I think this is the nameProperty of the file...
-        let scope = {}
-        let obj = visitors[Type.Compound].read(buffer, scope);
-        return { root: obj, schema: scope }
-    }
-
-    export function write(tag: Compound, compressed: boolean = false): Buffer {
-        const buffer = new ByteBuffer();
-        buffer.writeByte(Type.Compound);
-        writeUTF8(buffer, '');
-        writeTag(tag, buffer);
-        if (compressed) {
-            return gzip.gzipSync(Buffer.from(buffer.flip().toArrayBuffer()));
-        } else {
-            return Buffer.from(buffer.flip().toArrayBuffer());
-        }
-    }
-
-    function writeTag(tag: Base, buffer: ByteBuffer) {
-        if (tag.type === Type.Compound) {
-            const compound = <Compound><any>tag
-            for (const key of compound.keys()) {
-                const val = compound.value[key]
-                buffer.writeByte(val.type)
-                writeUTF8(buffer, key)
-                writeTag(val, buffer);
-            }
-            buffer.writeByte(0);
-        }
-        else if (tag.type === Type.List) {
-            const list = <List><any>tag;
-            const type = list.length === 0 ? 1 : list.value[0].type;
-            buffer.writeByte(type)
-            buffer.writeInt(list.length)
-            for (const val of list.value) {
-                if (val.type === type)
-                    writeTag(val, buffer)
-                else throw new Error(`Invalid type [${val.type}] for list with type [${type}]`)
-            }
-        } else
-            visitors[tag.type].write(buffer, tag.value, { find() { } })
-    }
-
-    export abstract class Base {
-        protected constructor(readonly type: NBT.Type, readonly value: any) { }
-        abstract toJSON(): object;
-    }
-
-    class Empty extends Base {
-        constructor() { super(Type.End, null) }
-        toJSON() { return {} }
-    }
-
-    export const EMPTY: Base = new Empty()
-
-    export class List extends Base {
-        constructor(list?: Base[]) {
-            super(Type.List, list ? list : []);
-        }
-        get length() { return this.value.length; }
-        get value(): Array<Base> { return this.value; }
-        toJSON() {
-            return JSON.parse(JSON.stringify(this))
-        }
-    }
-
-    export class Compound extends Base {
-        constructor(original?: Compound | { [key: string]: Base } | Map<string, Base>, deepcopy: boolean = false) {
-            super(Type.Compound, {})
-            let val: any;
-            if (original instanceof Compound) {
-                val = deepcopy ? JSON.parse(JSON.stringify(original.value)) : { ...original.value }
-            } else if (original instanceof Map) {
-                val = {}
-                original.forEach((v, k) => { val[k] = v; })
-            } else {
-                val = deepcopy ? JSON.parse(JSON.stringify(original)) : { ...original }
-            }
-            (this as any).value = val;
-        }
-
-        boolean(name: string, value: boolean): this { this.value[name] = new Primitive(Type.Byte, value); return this }
-        byte(name: string, value: number): this { this.value[name] = new Primitive(Type.Byte, value); return this; }
-        short(name: string, value: number): this { this.value[name] = new Primitive(Type.Short, value); return this; }
-        int(name: string, value: number): this { this.value[name] = new Primitive(Type.Int, value); return this; }
-        long(name: string, value: number | Long): this { this.value[name] = new Primitive(Type.Long, value); return this; }
-        float(name: string, value: number): this { this.value[name] = new Primitive(Type.Float, value); return this; }
-        double(name: string, value: number): this { this.value[name] = new Primitive(Type.Double, value); return this; }
-        string(name: string, value: string): this { this.value[name] = new Primitive(Type.String, value); return this }
-        bytes(name: string, value: number[]): this { this.value[name] = new Primitive(Type.ByteArray, value); return this; }
-        ints(name: string, value: number[]): this { this.value[name] = new Primitive(Type.IntArray, value); return this; }
-        list(name: string, value: Base[]): this {
-            if (value.length == 0) return this;
-            this.value[name] = new List(value)
-            return this;
-        }
-
-        nbt(name: string, value: Base): this { this.value[name] = value; return this }
-        get(name: string, fallback?: Base): Base | undefined {
-            const val = this.value[name];
-            return val ? val : fallback;
-        }
-        has(name: string): boolean { return this.value[name] !== undefined }
-        keys() { return Object.keys(this.value) }
-        [Symbol.iterator]() { return this.keys().map(k => this.value[k]) }
-        values() { return this.keys().map(k => this.value[k]) }
-        toJSON() {
-            return JSON.parse(JSON.stringify(this.value))
-        }
-    }
-    export class Primitive extends Base {
-        constructor(type: NBT.Type, value: string | number | boolean | number[] | Long) { super(type, value) }
-        toJSON() {
-            return this.value
-        }
-    }
 }
 export default NBT;
+
 
 function writeUTF8(out: ByteBuffer, str: string) {
     let strlen = str.length;
