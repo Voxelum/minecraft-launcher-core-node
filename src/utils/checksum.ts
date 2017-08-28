c09b39700a7097779b0b5d8caed17e17b6da6349
ci010 <cijhn@hotmail.com>
2017-08-28 15:17:00 +0800

    fix for better auto-import

--- src/utils/checksum.ts
+++ src/utils/checksum.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs-extra'
 import * as crypto from 'crypto';
 
-export default function (path: string, algorithm: string = 'sha1'): Promise<string> {
+export default function checksum(path: string, algorithm: string = 'sha1'): Promise<string> {
     return fs.readFile(path).then(data => crypto.createHash(algorithm).update(data).digest('hex'))
 }
