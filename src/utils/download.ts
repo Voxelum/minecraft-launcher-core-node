c26a0d4f1e57ad0d4a820a966e222d650f02c6f8
ci010 <cijhn@hotmail.com>
2017-09-11 17:15:38 +0800

    taskify

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -2,39 +2,57 @@ import * as fs from 'fs-extra'
 import * as http from 'http'
 import * as https from 'https'
 import * as urls from 'url'
-import { Writable } from 'stream';
+import { Writable, Readable } from 'stream';
 import * as ByteBuffer from "bytebuffer";
 
 type Requestor = {
     get(options: http.RequestOptions | string, callback?: (res: http.IncomingMessage) => void): http.ClientRequest;
 };
 
-let download: (url: string, file?: string) => Promise<Buffer | void>;
+export interface IncomingMessage extends Readable {
+    headers: any;
+    statusCode: number;
+    statusMessage: string;
+}
+
+function handleResponse(response: IncomingMessage, reject: (any?: any) => void, resolve: (any?: any) => void, file?: string, cb?: (progress: number, total: number) => void) {
+    if (response.statusCode !== 200) {
+        reject(new Error(response.statusCode.toString()))
+    }
+    let stream = file ? fs.createWriteStream(file) : new WriteableBuffer()
+    response.on('error', (e: Error) => {
+        if (file) fs.unlink(file, err => {
+            reject(e);
+        });
+        else reject(e)
+    })
+    if (cb) {
+        let len = 0;
+        const total = Number.parseInt(response.headers['content-length'])
+        cb(-1, total)
+        response.on('data', (buf: Buffer) => {
+            len += buf.length;
+            cb(len, total)
+        })
+    }
+    response.pipe(stream)
+    stream.on('finish', () => {
+        if (file) {
+            (stream as any).close();
+            resolve()
+        } else {
+            resolve((stream as WriteableBuffer).toBuffer())
+        }
+    })
+}
+let download: (url: string, file?: string, cb?: (progress: number, total: number) => void) => Promise<Buffer | void>;
 try {
-    const electron = require('electron')
-    const net = electron.net
-    download = (url: string, file?: string) => {
+    const net = require('electron').net
+    download = (url: string, file?: string, cb?: (progress: number, total: number) => void) => {
         const req = net.request(url);
         return new Promise<Buffer | void>((resolve, reject) => {
             req.on('response', (response: any) => {
-                if (response.statusCode !== 200) {
-                    reject(new Error(response.statusCode))
-                }
-                let stream = file ? fs.createWriteStream(file) : new WriteableBuffer()
-                response.on('error', (e: Error) => {
-                    if (file) fs.unlink(file, err => {
-                        reject(e);
-                    });
-                })
-                response.pipe(stream)
-                stream.on('finish', () => {
-                    if (file) {
-                        (stream as any).close();
-                        resolve()
-                    } else {
-                        resolve((stream as WriteableBuffer).toBuffer())
-                    }
-                })
+                handleResponse(response, reject, resolve, file, cb);
             })
             req.on('error', (err: any) => {
                 if (file) fs.unlink(file, e => {
@@ -73,36 +91,25 @@ catch (e) {
             req.end()
         })
     }
-    download = (url: string, file?: string): Promise<void | Buffer> => {
-        return findSource(url)
-            .then(res => {
-                if (res.statusCode !== 200)
-                    throw new Error(`Error ${res.statusCode}`)
-                else {
-                    let stream = file ? fs.createWriteStream(file) : new WriteableBuffer()
-                    return new Promise<void | Buffer>((resolve, reject) => {
-                        res.pipe(stream)
-                        res.on('error', (err) => {
-                            if (file) {
-                                (stream as any).close();
-                                fs.unlink(file, e => {
-                                    reject(err);
-                                });
-                            } else reject(err);
-                        })
-                        stream.on('finish', () => {
-                            if (file) {
-                                (stream as any).close(); resolve();
-                            } else {
-                                resolve((stream as WriteableBuffer).toBuffer())
-                            }
-                        })
-                    });
-                }
-            })
+    download = (url: string, file?: string, cb?: (progress: number, total: number) => void): Promise<void | Buffer> => {
+        return findSource(url).then(res => {
+            return new Promise<void | Buffer>((resolve, reject) => {
+                handleResponse(res as IncomingMessage, reject, resolve, file, cb);
+            });
+        })
     }
 }
 
+import Task, { AbstractTask } from './task'
+
+export class DownloadTask extends AbstractTask<void | Buffer> {
+    constructor(id: string, readonly url: string, readonly file?: string) { super(id); }
+    execture(context: Task.Context): Promise<void | Buffer> {
+        return download(this.url, this.file, (progress, total) => {
+            this.emit('update', progress, total);
+        })
+    }
+}
 class WriteableBuffer extends Writable {
     constructor(public buffer: ByteBuffer = new ByteBuffer()) {
         super();
---
+++ src/utils/task.ts
@@ -0,0 +1,55 @@
+import { EventEmitter } from 'events';
+
+export interface Task<T> extends EventEmitter {
+    readonly id: string;
+    on(event: 'error', listener: (error: Error) => void): this;
+    on(event: 'update', listener: (progres: number, total?: number, status?: string) => void): this;
+    on(event: 'finish', listener: () => void): this;
+    execute(context: Task.Context): Promise<T>
+}
+
+export class SimpleTask<T> extends EventEmitter implements Task<T> {
+    constructor(readonly id: string, readonly promise: (() => Promise<T>) | Promise<T>) { super(); }
+    execute(context: Task.Context): Promise<T> {
+        if (this.promise instanceof Promise) {
+            return this.promise;
+        } else {
+            return this.promise();
+        }
+    }
+}
+
+export abstract class AbstractTask<T> extends EventEmitter implements Task<T> {
+    readonly id: string;
+    constructor(id?: string) {
+        super();
+        this.id = id || this.constructor.name;
+    }
+    abstract execute(context: Task.Context): Promise<T>;
+}
+
+export namespace Task {
+    export interface Context {
+        execute<T>(task: Task<T>): Promise<T>;
+        wrap<T>(id: string, promise: (() => Promise<T>) | Promise<T>): Promise<T>;
+    }
+
+    export function wrap<T>(id: string, promise: (() => Promise<T>) | Promise<T>): Task<T> {
+        return new SimpleTask(id, promise);
+    }
+
+    export function execute<T>(task: Task<T>) {
+        return dummyContext.execute(task);
+    }
+    class DummyContext implements Context {
+        execute<T>(task: Task<T>): Promise<T> {
+            return task.execute(this)
+        }
+        wrap<T>(id: string, promise: Promise<T> | (() => Promise<T>)): Promise<T> {
+            return Task.wrap(id, promise).execute(this);
+        }
+    }
+    const dummyContext = new DummyContext()
+}
+
+export default Task;
