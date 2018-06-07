08d5a67a05431f2d8a259146242f924fb54f36c9
ci010 <cijhn@hotmail.com>
2018-06-07 15:48:36 -0700

    add more tests

--- src/tests/forge.test.ts
+++ src/tests/forge.test.ts
@@ -12,10 +12,47 @@ describe('Forge', () => {
         assert(meta);
     }).timeout(100000)
     it('should not fetch duplicate forge version', async () => {
-        const listContainer0 = await Forge.VersionMetaList.update();
-        const listContainer1 = await Forge.VersionMetaList.update({ fallback: listContainer0 });
-        assert.equal(listContainer0, listContainer1);
+        const src = await Forge.VersionMetaList.update();
+        const shouldNotSrc = await Forge.VersionMetaList.update();
+
+        const shouldSrc = await Forge.VersionMetaList.update({ fallback: src });
+        assert.equal(src, shouldSrc);
+        assert.notEqual(src, shouldNotSrc);
     }).timeout(100000)
+
+    it('should read exactly things in jar', async function () {
+        const metadata = await Forge.readModMetaData(`${this.assets}/sample-mod.jar`);
+        assert.equal(metadata.length, 3)
+    })
+    
+    it('should read mcmod.info in jar', async function () {
+        const metadata = await Forge.readModMetaData(`${this.assets}/sample-mod.jar`);
+        assert.equal(metadata[0].modid, 'soundfilters');
+        assert.equal(metadata[0].name, 'Sound Filters');
+        assert.equal(metadata[0].description, 'Adds reveb to caves, as well as muted sounds underwater/in lava, and behind walls.');
+        assert.equal(metadata[0].version, '0.8_for_1,8');
+        assert.equal(metadata[0].mcversion, '1.8');
+        assert.equal(metadata[0].credits, 'Made by Tmtravlr.');
+    })
+
+    it('should read @Mod in jar', async function () {
+        const metadata = await Forge.readModMetaData(`${this.assets}/sample-mod.jar`);
+        assert.equal(metadata[2].modid, 'NuclearCraft');
+        assert.equal(metadata[2].version, '1.9e');
+    })
+
+
+    it('should detect optifine from class in jar', async function () {
+        const metadata = await Forge.readModMetaData(`${this.assets}/sample-mod.jar`);
+        assert.equal(metadata[1].modid, 'OptiFine');
+        assert.equal(metadata[1].name, 'OptiFine');
+        assert.equal(metadata[1].description, 'OptiFine is a Minecraft optimization mod. It allows Minecraft to run faster and look better with full support for HD textures and many configuration options.');
+        assert.equal(metadata[1].version, 'HD_U_C5');
+        assert.equal(metadata[1].mcversion, '1.12.1');
+        assert.equal(metadata[1].url, 'https://optifine.net');
+    })
+
+
 
     describe('Config', () => {
         const cfg1 = `
@@ -57,6 +94,8 @@ describe('Forge', () => {
         it('forge config multiple properties parsing', () => {
             assert.equal(config['versioncheck'].properties[0].name, 'checkForUpdates')
             assert.equal(config['versioncheck'].properties[1].name, 'knownVersions')
+            assert.equal(config['versioncheck'].properties[1].value[0], 'InGameInfoXML 2.8.1.82')
+            assert.equal(config['versioncheck'].properties[1].value[1], 'LunatriusCore 1.1.2.21')
             assert.equal(config['versioncheck'].properties[2].name, 'silenceKnownUpdates')
             assert.equal(config['versioncheck'].properties[0].type, 'B')
             assert.equal(config['versioncheck'].properties[1].type, 'S')
