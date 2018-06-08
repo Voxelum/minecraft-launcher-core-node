6d77da7932ac1b60f514c8f2234dce1faa20a27e
ci010 <cijhn@hotmail.com>
2018-06-08 14:22:08 -0700

    fix liteloader parsing

--- src/tests/liteloader.test.ts
+++ src/tests/liteloader.test.ts
@@ -1,4 +1,4 @@
-import { LiteLoader, MinecraftFolder, Launcher } from '../index'
+import { LiteLoader, MinecraftFolder, Launcher } from '..'
 import * as assert from 'assert'
 
 describe('Liteloader', () => {
@@ -7,4 +7,13 @@ describe('Liteloader', () => {
             assert(list)
         })
     )
+    it('should be able to parse liteloader info', async function () {
+        const metadata = await LiteLoader.meta(`${this.assets}/sample-mod.litemod`)
+        assert.equal(metadata.name, 'Autofish')
+        assert.equal(metadata.mcversion, '1.7.10')
+        assert.deepEqual(metadata.revision, 39)
+        assert.equal(metadata.author, 'troyboy50')
+        assert.equal(metadata.classTransformerClasses, 'troy.autofish.PacketEntityVelocityTransformer')
+        assert.equal(metadata.version, 'Autofish:1.7.10_39')
+    })
 })
