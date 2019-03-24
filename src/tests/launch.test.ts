848e36b3942cc8130ee0b5b2e788816141deb302
ci010 <cijhn@hotmail.com>
2019-03-23 22:28:03 -0700

    skip test launch on travis;

--- src/tests/launch.test.ts
+++ src/tests/launch.test.ts
@@ -61,7 +61,7 @@ describe("Launch", () => {
             if (process.env.JAVA_HOME) {
                 javaPath = `${process.env.JAVA_HOME}/bin/java`;
                 if (process.env.ENV && process.env.ENV === "TRAVIS") {
-                    javaVersion = Number.MAX_VALUE;
+                    this.skip();
                 } else {
                     const javaVersionStr = await getJavaVersion(javaPath);
                     javaVersion = Number.parseFloat(javaVersionStr.split(EOL)[0].split(" ")[1].split(".")[0]);
@@ -91,104 +91,104 @@ describe("Launch", () => {
                 });
             });
         }).timeout(100000);
-        // it("should launch server", async function () {
-        //     const option: Launcher.Option = {
-        //         version: "1.12.2", gamePath: this.gameDirectory, javaPath, server: {
-        //             ip: "127.0.0.1",
-        //             port: 25565,
-        //         },
-        //     };
-        //     const proc = await Launcher.launch(option);
-        //     await new Promise((resol, rej) => {
-        //         proc.stdout.on("data", (chunk) => {
-        //             const str = chunk.toString();
-        //             if (str.indexOf("[Client thread/INFO]: Connecting to 127.0.0.1, 25565") !== -1) {
-        //                 proc.kill("SIGINT");
-        //             }
-        //         });
-        //         proc.stderr.on("data", (chunk) => {
-        //             console.log(chunk.toString());
-        //         });
-        //         proc.on("exit", (code, signal) => {
-        //             if (signal === "SIGINT") {
-        //                 resol();
-        //             } else { rej({ code, signal }); }
-        //         });
-        //     });
-        // }).timeout(100000);
-        // it("should launch forge minecraft", async function () {
-        //     if (javaVersion > 8) { this.skip(); }
-        //     const option = { version: "1.12.2-forge1.12.2-14.23.5.2823", gamePath: this.gameDirectory, javaPath };
-        //     const proc = await Launcher.launch(option);
-        //     await new Promise((resol, rej) => {
-        //         proc.stdout.on("data", (chunk) => {
-        //             const str = chunk.toString();
-        //             if (str.indexOf("[main/INFO] [FML]: Itemstack injection complete") !== -1) {
-        //                 proc.kill("SIGINT");
-        //             }
-        //             // console.log(chunk.toString())
-        //         });
-        //         proc.stderr.on("data", (chunk) => {
-        //             console.log(chunk.toString());
-        //         });
-        //         proc.on("exit", (code, signal) => {
-        //             if (signal === "SIGINT") {
-        //                 resol();
-        //             } else { rej({ code, signal }); }
-        //         });
-        //     });
-        // }).timeout(100000);
-        // it("should launch liteloader minecraft", async function () {
-        //     if (javaVersion > 8) { this.skip(); }
-        //     const option = { version: "1.12.2-Liteloader1.12.2-1.12.2-SNAPSHOT", gamePath: this.gameDirectory, javaPath };
-        //     const proc = await Launcher.launch(option);
-        //     await new Promise((resol, rej) => {
-        //         const out: string[] = [];
-        //         proc.stdout.on("data", (chunk) => {
-        //             const str = chunk.toString();
-        //             out.push(str);
-        //             if (str.indexOf("LiteLoader begin POSTINIT") !== -1) {
-        //                 proc.kill("SIGINT");
-        //             }
-        //         });
-        //         proc.stderr.on("data", (chunk) => {
-        //             out.push(chunk.toString());
-        //         });
-        //         proc.on("exit", (code, signal) => {
-        //             if (signal === "SIGINT") {
-        //                 resol();
-        //             } else { rej({ code, signal, output: out.join("\n") }); }
-        //         });
-        //     });
-        // }).timeout(100000);
+        it("should launch server", async function () {
+            const option: Launcher.Option = {
+                version: "1.12.2", gamePath: this.gameDirectory, javaPath, server: {
+                    ip: "127.0.0.1",
+                    port: 25565,
+                },
+            };
+            const proc = await Launcher.launch(option);
+            await new Promise((resol, rej) => {
+                proc.stdout.on("data", (chunk) => {
+                    const str = chunk.toString();
+                    if (str.indexOf("[Client thread/INFO]: Connecting to 127.0.0.1, 25565") !== -1) {
+                        proc.kill("SIGINT");
+                    }
+                });
+                proc.stderr.on("data", (chunk) => {
+                    console.log(chunk.toString());
+                });
+                proc.on("exit", (code, signal) => {
+                    if (signal === "SIGINT") {
+                        resol();
+                    } else { rej({ code, signal }); }
+                });
+            });
+        }).timeout(100000);
+        it("should launch forge minecraft", async function () {
+            if (javaVersion > 8) { this.skip(); }
+            const option = { version: "1.12.2-forge1.12.2-14.23.5.2823", gamePath: this.gameDirectory, javaPath };
+            const proc = await Launcher.launch(option);
+            await new Promise((resol, rej) => {
+                proc.stdout.on("data", (chunk) => {
+                    const str = chunk.toString();
+                    if (str.indexOf("[main/INFO] [FML]: Itemstack injection complete") !== -1) {
+                        proc.kill("SIGINT");
+                    }
+                    // console.log(chunk.toString())
+                });
+                proc.stderr.on("data", (chunk) => {
+                    console.log(chunk.toString());
+                });
+                proc.on("exit", (code, signal) => {
+                    if (signal === "SIGINT") {
+                        resol();
+                    } else { rej({ code, signal }); }
+                });
+            });
+        }).timeout(100000);
+        it("should launch liteloader minecraft", async function () {
+            if (javaVersion > 8) { this.skip(); }
+            const option = { version: "1.12.2-Liteloader1.12.2-1.12.2-SNAPSHOT", gamePath: this.gameDirectory, javaPath };
+            const proc = await Launcher.launch(option);
+            await new Promise((resol, rej) => {
+                const out: string[] = [];
+                proc.stdout.on("data", (chunk) => {
+                    const str = chunk.toString();
+                    out.push(str);
+                    if (str.indexOf("LiteLoader begin POSTINIT") !== -1) {
+                        proc.kill("SIGINT");
+                    }
+                });
+                proc.stderr.on("data", (chunk) => {
+                    out.push(chunk.toString());
+                });
+                proc.on("exit", (code, signal) => {
+                    if (signal === "SIGINT") {
+                        resol();
+                    } else { rej({ code, signal, output: out.join("\n") }); }
+                });
+            });
+        }).timeout(100000);
 
-        // it("should launch forge liteloader minecraft", async function () {
-        //     if (javaVersion > 8) { this.skip(); }
-        //     const option = { version: "1.12.2-forge1.12.2-14.23.5.2823-Liteloader1.12.2-1.12.2-SNAPSHOT", gamePath: this.gameDirectory, javaPath };
-        //     const proc = await Launcher.launch(option);
-        //     await new Promise((resol, rej) => {
-        //         let foundLite = false;
-        //         let foundForge = false;
-        //         const out: string[] = [];
-        //         proc.stdout.on("data", (chunk) => {
-        //             const str = chunk.toString();
-        //             out.push(str);
-        //             if (str.indexOf("LiteLoader begin POSTINIT") !== -1) { foundLite = true; }
-        //             if (str.indexOf("[main/INFO] [FML]: Itemstack injection complete") !== -1) { foundForge = true; }
-        //             if (foundForge && foundLite) {
-        //                 proc.kill("SIGINT");
-        //             }
-        //         });
-        //         proc.stderr.on("data", (chunk) => {
-        //             out.push(chunk.toString());
-        //         });
-        //         proc.on("exit", (code, signal) => {
-        //             if (signal === "SIGINT") {
-        //                 resol();
-        //             } else { rej({ code, signal, output: out.join("\n") }); }
-        //         });
-        //     });
-        // }).timeout(100000);
+        it("should launch forge liteloader minecraft", async function () {
+            if (javaVersion > 8) { this.skip(); }
+            const option = { version: "1.12.2-forge1.12.2-14.23.5.2823-Liteloader1.12.2-1.12.2-SNAPSHOT", gamePath: this.gameDirectory, javaPath };
+            const proc = await Launcher.launch(option);
+            await new Promise((resol, rej) => {
+                let foundLite = false;
+                let foundForge = false;
+                const out: string[] = [];
+                proc.stdout.on("data", (chunk) => {
+                    const str = chunk.toString();
+                    out.push(str);
+                    if (str.indexOf("LiteLoader begin POSTINIT") !== -1) { foundLite = true; }
+                    if (str.indexOf("[main/INFO] [FML]: Itemstack injection complete") !== -1) { foundForge = true; }
+                    if (foundForge && foundLite) {
+                        proc.kill("SIGINT");
+                    }
+                });
+                proc.stderr.on("data", (chunk) => {
+                    out.push(chunk.toString());
+                });
+                proc.on("exit", (code, signal) => {
+                    if (signal === "SIGINT") {
+                        resol();
+                    } else { rej({ code, signal, output: out.join("\n") }); }
+                });
+            });
+        }).timeout(100000);
 
         describe("1.13.2", function () {
             it("should launch normal minecraft", async function () {
