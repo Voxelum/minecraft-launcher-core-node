dc37b4e3c94fd3ae3c3249f1642f1961badde4c5
ci010 <cijhn@hotmail.com>
2019-10-27 12:33:19 +0800

    feat(world): poc of read region data

--- packages/world/index.ts
+++ packages/world/index.ts
@@ -1,10 +1,9 @@
-import { LevelDataFrame, RegionDataFrame, World } from "@xmcl/common";
+import { LevelDataFrame, PlayerDataFrame, RegionDataFrame, World } from "@xmcl/common";
 import { NBT } from "@xmcl/nbt";
 import Unzip from "@xmcl/unzip";
 import { vfs } from "@xmcl/util";
 import fileType from "file-type";
 import * as path from "path";
-import { inflate } from "zlib";
 
 declare module "@xmcl/common/world" {
     namespace World {
@@ -26,6 +25,151 @@ declare module "@xmcl/common/world" {
     }
 }
 
+export abstract class WorldReader {
+    public static async create(location: string) {
+        if (await vfs.stat(location).then((s) => s.isDirectory())) {
+            return new DirWorld(location);
+        } else {
+            const buffer = await vfs.readFile(location);
+            const ft = fileType(buffer);
+            if (ft && ft.ext === "zip") {
+                return new ZipWorld(location, await Unzip.open(location, { lazyEntries: false }));
+            }
+        }
+        throw new Error(`Unsupported world file! It should be either dir or zip. ${location}`);
+    }
+
+    public abstract readonly filePath: string;
+    private levelData?: LevelDataFrame;
+    private playerData?: PlayerDataFrame;
+
+    private regionMap: { [k: string]: RegionDataFrame } = {};
+
+    public async getRegionData(chunkX: number, chunkZ: number): Promise<RegionDataFrame> {
+        throw new Error();
+    }
+
+    public async getPlayerData(): Promise<PlayerDataFrame> {
+        if (this.playerData === null) {
+            this.playerData = await this.readEntry("players.dat").then((b) => NBT.Persistence.deserialize(b) as any);
+        }
+        return this.playerData!;
+    }
+
+    public async getLevelData(): Promise<LevelDataFrame> {
+        if (this.levelData === null) {
+            this.levelData = await this.readEntry("level.dat").then((b) => NBT.Persistence.deserialize(b) as any);
+        }
+        return this.levelData!;
+    }
+
+    protected abstract readEntry(relativePath: string): Promise<Buffer>;
+    protected abstract hasEntry(relativePath: string): Promise<boolean>;
+    protected abstract listEntries(relativePath: string): Promise<string[]>;
+}
+
+function seek(bs: Long[], index: number) {
+    function mask(bit: number) {
+        let m = 0;
+        for (let i = 0; i < bit; ++i) { m = (m << 1) + 1; }
+        return m;
+    }
+
+    const bitLen = bs.length * 64 / 4096;
+    const bitMask = mask(bitLen);
+
+    const bitOff = index * bitLen;
+    const tagAt = Math.floor(bitOff / 64);
+    const tag = bs[tagAt];
+
+    let bitStart = bitOff % 64;
+    const onLowBit = bitStart >= 32;
+    if (onLowBit) {
+        bitStart = bitStart - 32;
+    }
+    const value = onLowBit ? tag.low : tag.high;
+
+    const off = 32 - (bitStart + bitLen);
+    const overflow = off < 0;
+
+    let id: number;
+    if (overflow) {
+        const curBits = 32 - bitStart;
+        const nextBits = bitLen - curBits;
+        const partialMask = mask(curBits);
+        const nextValue = onLowBit
+            ? bs[tagAt + 1].high // use next long high
+            : tag.low; // use this low
+        id = ((value & partialMask) << nextBits) | (nextValue >>> (32 - nextBits));
+    } else {
+        id = (value >>> off) & bitMask;
+    }
+
+    return id;
+}
+
+export class RegionReader {
+    constructor(readonly region: RegionDataFrame) {
+    }
+
+    public getBlockStateAt(x: number, y: number, z: number): undefined | { Name: string; properties: { [key: string]: string } } {
+        const innerX = x % 16;
+        const innerZ = z % 16;
+
+        const chunkY = y << 4;
+
+        const section = this.region.Level.Sections[chunkY];
+
+        return this.at(section, ((innerX & 15) << 4) | (innerZ & 15));
+    }
+
+    private at(section: RegionDataFrame["Level"]["Sections"][number], index: number) {
+        if ("BlockStates" in section) {
+            return section.Palette[seek(section.BlockStates, index)];
+        }
+        return undefined;
+    }
+}
+
+class DirWorld extends WorldReader {
+    constructor(readonly filePath: string) {
+        super();
+    }
+
+    protected hasEntry(relativePath: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    protected listEntries(relativePath: string): Promise<string[]> {
+        throw new Error("Method not implemented.");
+    }
+    protected async readEntry(relativePath: string) {
+        const entry = path.join(this.filePath, relativePath);
+        if (!entry) { throw new Error(`Cannot find path ${relativePath}`); }
+        const buf = await vfs.readFile(entry);
+        return buf;
+    }
+}
+
+class ZipWorld extends WorldReader {
+    constructor(readonly filePath: string, readonly file: Unzip.CachedZipFile) {
+        super();
+    }
+
+    protected hasEntry(relativePath: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    protected listEntries(relativePath: string): Promise<string[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    protected async readEntry(relativePath: string) {
+        const entry = this.file.entries[relativePath];
+        if (!entry) { throw new Error(`Cannot find path ${relativePath}`); }
+        const buf = await this.file.readEntry(entry);
+        return buf;
+    }
+}
+
 World.getRegionFile = getRegionFile;
 World.load = load;
 World.loadRegionFromBuffer = loadRegionFromBuffer;
@@ -38,17 +182,18 @@ async function loadRegionFromBuffer(buffer: Buffer, x: number, z: number) {
     if (format !== 2) {
         throw new Error(`Cannot resolve chunk with format ${format}.`);
     }
+
     const chunkData = buffer.slice(off + 5, off + 5 + length);
-    const inflatedData = await new Promise<Buffer>((resolve, reject) => {
-        inflate(chunkData, (e, r) => {
-            if (e) {
-                reject(e);
-            } else {
-                resolve(r);
-            }
-        });
-    });
-    const data = await NBT.Persistence.deserialize(inflatedData, false);
+    // const inflatedData = await new Promise<Buffer>((resolve, reject) => {
+    //     inflate(chunkData, (e, r) => {
+    //         if (e) {
+    //             reject(e);
+    //         } else {
+    //             resolve(r);
+    //         }
+    //     });
+    // });
+    const data = await NBT.Persistence.deserialize(chunkData, true);
     return data as unknown as RegionDataFrame;
 }
 
