f9553f6547d8d67b59e2567824593b677c2627d9
ci010 <cijhn@hotmail.com>
2019-10-31 11:04:11 +0800

    feat(common): add packmeta and model definition

--- packages/common/README.md
+++ packages/common/README.md
@@ -5,3 +5,13 @@
 [![Build Status](https://github.com/voxelum/minecraft-launcher-core-node/workflows/Release%20Pre-Check/badge.svg)](https://github.com/voxelum/minecraft-launcher-core-node/workflows/Release%20Pre-Check/badge.svg)
 
 This is a sub-module belong to [minecraft-launcher-core](https://www.npmjs.com/package/@xmcl/minecraft-launcher-core) module. You can still use this individually.
+
+### Usage
+
+This module majorly provide typescript type definitions for many Minecraft concepts. The docs of them are majorly from minecraft wiki. 
+
+- format of text component - `TextComponentFrame` 
+- format for resourcepack pack meta - `PackMeta`
+- format of minecraft block model - `BlockModel`
+- format of minecraft game setting - `GameSetting`
+- format of minecraft save files (level, region) - `LevelDataFrame` and `RegionDataFrame` 
---
+++ packages/common/model.ts
@@ -0,0 +1,160 @@
+export type Vec3 = [number, number, number];
+export type Vec4 = [number, number, number, number];
+
+export namespace BlockModel {
+    export type Direction = "up" | "down" | "north" | "south" | "west" | "east";
+
+    export interface Display {
+        thirdperson_righthand: Transform;
+        thirdperson_lefthand: Transform;
+        firstperson_righthand: Transform;
+        firstperson_lefthand: Transform;
+        gui: Transform;
+        head: Transform;
+        ground: Transform;
+        fixed: Transform;
+    }
+    export interface Element {
+        /**
+         * Start point of a cube according to the scheme [x, y, z]. Values must be between -16 and 32.
+         */
+        from: Vec3;
+        /**
+         * Stop point of a cube according to the scheme [x, y, z]. Values must be between -16 and 32.
+         */
+        to: Vec3;
+        /**
+         * Defines the rotation of an element.
+         */
+        rotation?: {
+            /**
+             * Sets the center of the rotation according to the scheme [x, y, z], defaults to [8, 8, 8].
+             */
+            origin: Vec3;
+            /**
+             * Specifies the direction of rotation, can be "x", "y" or "z".
+             */
+            axis: "x" | "y" | "z";
+            /**
+             * Specifies the angle of rotation. Can be 45 through -45 degrees in 22.5 degree increments. Defaults to 0.
+             */
+            angle: number;
+            /**
+             * Specifies whether or not to scale the faces across the whole block. Can be true or false. Defaults to false.
+             */
+            rescale: boolean;
+        };
+        /**
+         * Defines if shadows are rendered (true - default), not (false).
+         */
+        shade?: boolean;
+        faces?: {
+            up?: Face;
+            down?: Face;
+            north?: Face;
+            south?: Face;
+            east?: Face;
+            west?: Face;
+        };
+    }
+    export interface Face {
+        /**
+         * Defines the area of the texture to use according to the scheme [x1, y1, x2, y2].
+         * If unset, it defaults to values equal to xyz position of the element.
+         * The texture behavior will be inconsistent if UV extends below 0 or above 16.
+         * If the numbers of x1 and x2 are swapped (e.g. from 0, 0, 16, 16 to 16, 0, 0, 16), the texture will be flipped. UV is optional, and if not supplied it will automatically generate based on the element's position.
+         */
+        uv?: Vec4;
+
+        /**
+         * Specifies the texture in form of the texture variable prepended with a #.
+         */
+        texture: string;
+        /**
+         * Specifies whether a face does not need to be rendered when there is a block touching it in the specified position. 
+         * The position can be: down, up, north, south, west, or east. It will also determine which side of the block to use the light level from for lighting the face, 
+         * and if unset, defaults to the side.
+         */
+        cullface?: Direction;
+
+        /**
+         * Rotates the texture by the specified number of degrees. 
+         * Can be 0, 90, 180, or 270. Defaults to 0. Rotation does not affect which part of the texture is used. 
+         * Instead, it amounts to permutation of the selected texture vertexes (selected implicitly, or explicitly though uv).
+         */
+        rotation?: 0 | 90 | 180 | 270;
+        /**
+         * Determines whether to tint the texture using a hardcoded tint index. The default is not using the tint, and any number causes it to use tint. Note that only certain blocks have a tint index, all others will be unaffected.
+         */
+        tintindex?: number;
+    }
+    export interface Transform {
+        /**
+         * Specifies the rotation of the model according to the scheme [x, y, z].
+         */
+        rotation: Vec3;
+        /**
+         *  Specifies the position of the model according to the scheme [x, y, z]. If the value is greater than 80, it is displayed as 80. If the value is less then -80, it is displayed as -80.
+         */
+        translation: Vec3;
+        /**
+         * Specifies the scale of the model according to the scheme [x, y, z]. If the value is greater than 4, it is displayed as 4.
+         */
+        scale: Vec3;
+    }
+    export type Resolved = Omit<Required<BlockModel>, "parent" | "override"> & { overrides?: BlockModel["overrides"] };
+}
+
+export interface BlockModel {
+    /**
+     * For Block:
+     *
+     * Loads a different model from the given path, starting in assets/minecraft/models. If both "parent" and "elements" are set, the "elements" tag overrides the "elements" tag from the previous model.
+     * Can be set to "builtin/generated" to use a model that is created out of the specified icon. Note that only the first layer is supported, and rotation can only be achieved using block states files.
+     *
+     * For Item:
+     *
+     * Loads a different model from the given path, starting in assets/minecraft/models. If both "parent" and "elements" are set, the "elements" tag overrides the "elements" tag from the previous model.
+     * Can be set to "builtin/generated" to use a model that is created out of the specified icon.
+     * Can be set to "builtin/entity" to load a model from an entity file. As you can not specify the entity, this does not work for all items (only for chests, ender chests, mob heads, shields and banners).
+     * Needs to be set to "builtin/compass" or "builtin/clock" for the compass and the clock.
+     */
+    parent?: string;
+
+    ambientocclusion?: boolean;
+    /**
+     * Holds the different places where item models are displayed.
+     */
+    display?: BlockModel.Display;
+    /**
+     * Holds the textures of the model. Each texture starts in assets/minecraft/textures or can be another texture variable.
+     */
+    textures?: {
+        /**
+         * What texture to load particles from. This texture is used if you are in a nether portal. Note: All breaking particles from non-model blocks are hard-coded.
+         */
+        particle?: string;
+        [variant: string]: string | undefined;
+    };
+
+    /**
+     * Contains all the elements of the model. they can only have cubic forms. If both "parent" and "elements" are set, the "elements" tag overrides the "elements" tag from the previous model.
+     */
+    elements?: BlockModel.Element[];
+    /**
+     * Determines cases which a different model should be used based on item tags.
+     * All cases are evaluated in order from top to bottom and last predicate that mathches will override.
+     * However, overrides are ignored if it has been already overriden once, for example this avoids recursion on overriding to the same model.
+     */
+    overrides?: Array<{
+        /**
+         * predicate: Holds the cases.
+         */
+        prediction: { [attribute: string]: number };
+        /**
+         * The path to the model to use if the case is met, starting in assets/minecraft/models/
+         */
+        model: string;
+    }>;
+}
+
