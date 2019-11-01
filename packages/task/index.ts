aa7a767dc987b7fb689c9b57249e629c93ac3e92
ci010 <cijhn@hotmail.com>
2019-11-01 20:08:47 +0800

    feat(task): new task api

--- packages/task/index.ts
+++ packages/task/index.ts
@@ -2,45 +2,53 @@ import { EventEmitter } from "events";
 
 export type TaskNode = Task.State;
 
-function execute<T>(context: Task.Context, work: Task.Work<T>): Promise<T> {
+function runTask<T>(context: Task.Context, task: Task<T>): Promise<T> {
     try {
-        const result = work(context);
+        const result = typeof task === "function" ? task(context) : task.run(context);
         return result instanceof Promise ? result : Promise.resolve(result);
     } catch (e) {
         return Promise.reject(e);
     }
 }
 
-export interface Task<T, N = Task.State> extends EventEmitter {
-    readonly root: N;
-    readonly work: Task.Work<T>;
+export interface TaskRuntime<N extends Task.State = Task.State> extends EventEmitter {
+    readonly factory: Task.StateFactory<N>;
 
-    on(event: "child", listener: (parentNode: N, childNode: N) => void): this;
-    on(event: "node-error", listener: (error: any, childNode: N) => void): this;
-    on(event: "update", listener: (update: { progress: number, total?: number, message?: string }, childNode: N) => void): this;
-    on(event: "finish", listener: (result: any, childNode: N) => void): this;
+    on(event: "execute", listener: (node: N, parent?: N) => void): this;
+    on(event: "node-error", listener: (error: any, node: N) => void): this;
+    on(event: "update", listener: (update: { progress: number, total?: number, message?: string }, node: N) => void): this;
+    on(event: "finish", listener: (result: any, node: N) => void): this;
     on(event: "pause", listener: (node: N) => void): this;
     on(event: "resume", listener: (node: N) => void): this;
-    on(event: "cancel", listener: () => void): this;
+    on(event: "cancel", listener: (node: N) => void): this;
 
-    once(event: "child", listener: (parentNode: N, childNode: N) => void): this;
-    once(event: "node-error", listener: (error: any, childNode: N) => void): this;
-    once(event: "update", listener: (update: { progress: number, total?: number, message?: string }, childNode: N) => void): this;
-    once(event: "finish", listener: (result: any, childNode: N) => void): this;
+    once(event: "execute", listener: (node: N, parent?: N) => void): this;
+    once(event: "node-error", listener: (error: any, node: N) => void): this;
+    once(event: "update", listener: (update: { progress: number, total?: number, message?: string }, node: N) => void): this;
+    once(event: "finish", listener: (result: any, node: N) => void): this;
     once(event: "pause", listener: (node: N) => void): this;
     once(event: "resume", listener: (node: N) => void): this;
-    once(event: "cancel", listener: () => void): this;
+    once(event: "cancel", listener: (node: N) => void): this;
 
-    execute(): Promise<T>;
-
-    pause(): void;
-    resume(): void;
-
-    cancel(): void;
+    /**
+     * Submit the task to task runtime. The runtime will decide when to run the task
+     * @param task The task will be run
+     */
+    submit<T>(task: Task<T, N>): TaskHandle<T, N>;
 }
 
-class TaskImpl<T, N extends Task.State> extends EventEmitter implements Task<T, N> {
-    readonly root: N;
+class RuntimeImpl<N extends Task.State = Task.State> extends EventEmitter implements TaskRuntime<N>  {
+    constructor(readonly factory: Task.StateFactory<N>) { super(); }
+
+    submit<T>(task: Task<T, N>) {
+        const handle = new TaskHandle(task, this, this.factory);
+        return handle;
+    }
+}
+
+export type Task<T, N = Task.State> = Task.Function<T> | Task.Object<T>;
+
+export class TaskHandle<T, N extends Task.State> {
     // tslint:disable: variable-name
     private _cancelled = false;
     private _promise: Promise<T> = new Promise<T>((resolve, reject) => {
@@ -50,182 +58,170 @@ class TaskImpl<T, N extends Task.State> extends EventEmitter implements Task<T, 
     private _reject: any;
     private _resolve: any;
     private _paused: boolean = false;
+
+    private _started: boolean = false;
+    private _resumeCallback: null | (() => void) = null;
+    private _onPause: null | (() => void) = null;
+    private _onResume: null | (() => void) = null;
+
+    private root: N;
     // tslint:enable: variable-name
 
-    constructor(nameOrObject: string | { name: string, arguments: object }, readonly work: Task.Work<T>, private factory: (state: Task.State) => N) {
-        super();
-        let args: object | undefined;
-        let name;
-        if (typeof nameOrObject === "string") { name = nameOrObject; } else {
-            name = nameOrObject.name;
-            args = nameOrObject.arguments;
-        }
-        this.root = factory({
-            name,
-            arguments: args,
-            path: name,
-            total: -1,
-            progress: -1,
-            children: [],
-            error: undefined,
-            status: "ready",
-            message: "",
+    constructor(readonly task: Task<T>,
+        readonly runtime: EventEmitter,
+        private factory: (state: Task.State) => N) {
+        this.root = this.factory({
+            name: this.task.name,
+            arguments: this.task.parameters,
+            path: this.task.name,
         });
+        this.executeOnNode(
+            this.createContext(this.root),
+            this.task,
+            this.root).then((r) => this._resolve(r), (e) => this._reject(e));
     }
 
-    createContext(node: Task.State): Task.Context {
-        const self = this;
-        return {
-            get task() {
-                return self;
-            },
-            update(progress: number, total?: number, message?: string) {
-                node.progress = progress;
-                node.total = total || node.total;
-                node.message = message || node.message;
-                self.emit("update", { progress, total, message }, node);
-                if (self._cancelled) {
-                    node.status = "cancelled";
-                    throw new Task.CancelledError();
-                }
-            },
-            execute<X>(nameOrObject: string | { name: string, arguments: object }, work: Task.Work<X>): Promise<X> {
-                let name;
-                let args: object | undefined;
-                if (typeof nameOrObject === "string") { name = nameOrObject; } else {
-                    name = nameOrObject.name;
-                    args = nameOrObject.arguments;
-                }
-                if (node.arguments) {
-                    if (args) {
-                        args = { ...node.arguments, ...args };
-                    } else {
-                        args = node.arguments;
-                    }
-                }
-
-                const nextPath = node.path + "." + name;
-                const nextNode: N = self.factory({
-                    name,
-                    arguments: args,
-                    path: nextPath,
-                    total: -1,
-                    progress: -1,
-                    children: [],
-                    error: undefined,
-                    status: "ready",
-                    message: "",
-                });
-                self.emit("child", node, nextNode);
-                const context = self.createContext(nextNode);
-
-                return self.executeOnNode(context, work, nextNode);
-            },
-        };
-    }
-
-    execute(): Promise<T> {
-        if (this.root.status !== "ready") { return this._promise; }
-        this.executeOnNode(this.createContext(this.root), this.work, this.root)
-            .then((r) => this._resolve(r), (e) => this._reject(e));
+    wait(): Promise<T> {
         return this._promise;
     }
 
     cancel(): void {
         this._cancelled = true;
-        this.emit("cancel");
     }
+
+    get isCancelled() { return this._cancelled; }
+
+    get isPaused() { return this._paused; }
 
     pause() {
         this._paused = true;
-        this.emit("pause");
+        if (this._onPause) { this._onPause(); }
     }
 
     resume() {
         this._paused = false;
-        this.emit("resume");
+        if (this._resumeCallback) {
+            this._resumeCallback();
+        }
+        if (this._onResume) {
+            this._onResume();
+        }
+        this.runtime.emit("resume", this.root);
+    }
+
+    private createContext(node: Task.State): Task.Context {
+        const self = this;
+        const runtime = this.runtime;
+        return {
+            pausealbe(onPause, onResume) {
+                self._onPause = onPause || null;
+                self._onResume = onResume || null;
+            },
+            update(progress: number, total?: number, message?: string) {
+                runtime.emit("update", { progress, total, message }, node);
+                if (self._cancelled) {
+                    runtime.emit("cancelled", node);
+                    throw new Task.CancelledError();
+                }
+            },
+            execute<X>(task: Task<X>): Promise<X> {
+                const name = task.name;
+                const args: object | undefined = task.parameters;
+
+                const nextPath = node.path + "." + name;
+                const nextNode: N = self.factory({
+                    name,
+                    arguments: Object.assign({}, node.arguments, args || {}),
+                    path: nextPath,
+                });
+                const context = self.createContext(nextNode);
+
+                return self.executeOnNode(context, task, nextNode, node);
+            },
+        };
     }
 
     private async waitIfPause() {
         if (this._paused) {
-            await new Promise<void>((resolve, reject) => {
-                this.once("resume", () => resolve());
+            await new Promise<void>((resolve) => {
+                this._resumeCallback = () => {
+                    this._resumeCallback = null;
+                    resolve();
+                };
             });
         }
     }
 
-    private async executeOnNode<CT>(context: Task.Context, work: Task.Work<CT>, node: Task.State) {
+    private async executeOnNode<CT>(context: Task.Context, work: Task<CT>, node: Task.State, parent?: Task.State) {
+        await new Promise((resolve) => setImmediate(() => resolve()));
         if (this._cancelled) {
-            node.status = "cancelled";
+            this.runtime.emit("cancel", node);
             throw new Task.CancelledError();
         }
 
         if (this._paused) {
-            node.status = "paused";
+            this.runtime.emit("pause", node);
         }
 
         await this.waitIfPause();
 
-        node.status = "running";
-        return execute(context, work).then((r) => {
-            node.status = "successed";
-            this.emit("finish", r, node);
+        this._started = true;
+
+        this.runtime.emit("execute", node, parent);
+        return runTask(context, work).then((r) => {
+            this.runtime.emit("finish", r, node);
             return r;
         }, (e) => {
             if (e instanceof Task.CancelledError) {
-                node.status = "cancelled";
+                this.runtime.emit("cancel", node);
                 throw e;
             }
-            node.status = "failed";
-            node.error = e;
-            this.emit("node-error", e, node);
+            this.runtime.emit("node-error", e, node);
             throw e;
         });
     }
 }
 
 export namespace Task {
+    export interface Function<T> {
+        readonly name: string;
+        readonly parameters?: object;
+        (context: Task.Context): (Promise<T> | T);
+    }
+    export interface Object<T> {
+        readonly name: string;
+        readonly parameters?: object;
+        readonly run: (context: Task.Context) => (Promise<T> | T);
+    }
     export class CancelledError extends Error {
     }
 
-    export type Status = "ready" | "running" | "successed" | "failed" | "cancelled" | "paused";
     export interface Context {
-        readonly task: Task<any>;
-
+        pausealbe(onPause?: () => void, onResume?: () => void): void;
         update(progres: number, total?: number, message?: string): void;
-        execute<T>(name: string, work: Work<T>): Promise<T>;
-        execute<T>(object: { name: string, arguments: object }, work: Work<T>): Promise<T>;
+        execute<T>(task: Task<T>): Promise<T>;
     }
-
-    export type Work<T> = (context: Task.Context) => (Promise<T> | T);
 
     export type StateFactory<X extends Task.State = Task.State> = (node: Task.State) => X;
 
     export const DEFAULT_STATE_FACTORY: StateFactory = (n) => n;
 
-    let usingStateFactory: StateFactory = DEFAULT_STATE_FACTORY;
-
     /**
-     * Change the default factory of state
+     * Run the task immediately (next tick)
+     * @param task The task will be run
      */
-    export function useStateFactory<T extends Task.State>(factory: StateFactory<T>) {
-        usingStateFactory = factory;
+    export function execute<T, N extends Task.State = State>(task: Task<T, N>) {
+        return new TaskHandle(task, { emit() { } } as any, DEFAULT_STATE_FACTORY).wait();
     }
 
-    export function create<T, N extends Task.State = Task.State>(name: string | { name: string, arguments: object }, work: Work<T>, stateFactory: StateFactory<N> = usingStateFactory as StateFactory<N>): Task<T> {
-        return new TaskImpl<T, N>(name, work, stateFactory);
+    export function createRuntime<X extends Task.State = Task.State>(factory: StateFactory<X> = DEFAULT_STATE_FACTORY as any): TaskRuntime {
+        return new RuntimeImpl(factory);
     }
 
     export interface State {
         name: string;
         arguments?: object;
-        total: number;
-        progress: number;
-        status: Status;
         path: string;
-        children: State[];
-        error: any;
-        message: string;
     }
 }
 
