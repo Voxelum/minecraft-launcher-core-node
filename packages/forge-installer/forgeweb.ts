4b6bb323b223ea633cf6e9e458dbc701caa7142f
ci010 <cijhn@hotmail.com>
2019-11-04 14:41:32 +0800

    fix(forgeweb): optimize the grab info strategy (hopefully)
    fix #43

--- packages/forge-installer/forgeweb.ts
+++ packages/forge-installer/forgeweb.ts
@@ -1,4 +1,4 @@
-import { UpdatedObject, getIfUpdate } from "@xmcl/net";
+import { getIfUpdate, UpdatedObject } from "@xmcl/net";
 import * as parser from "fast-html-parser";
 import { ForgeInstaller } from "./index";
 
@@ -26,13 +26,44 @@ export namespace ForgeWebPage {
                         .filter((elem) => elem.tagName === "li")
                         .map((elem) => {
                             elem = elem.removeWhitespace();
-                            const tt = elem.querySelector(".info-tooltip");
-                            const url = tt.querySelector("a") || elem.querySelector("a");
+                            /*
+                             * <div class="info-tooltip">
+                             *   <strong>MD5:</strong> 31742b6c996f53af96f606b7a0c46e2a<br>
+                             *   <strong>SHA1:</strong> 8d6a23554839d6f6014fbdb7991e3cd8af7eca80
+                             * </div>
+                             */
+                            const tooltipInfo = elem.querySelector(".info-tooltip");
+                            const url = tooltipInfo.querySelector("a") || elem.querySelector("a");
+                            // href is like /maven/net/minecraftforge/forge/1.14.4-28.1.70/forge-1.14.4-28.1.70-changelog.txt
+                            const href = url.attributes.href.trim();
+                            const matched = /forge-.+-.+-(\w+)\.\w+/.exec(href);
+                            let name = "", sha1 = "", md5 = "";
+                            if (matched) { name = matched[1]; }
+                            if (!name) {
+                                throw new Error(`Cannot determine name for forge url "${href}". Maybe the forge webisite changed?`);
+                            }
+                            try {
+                                md5 = tooltipInfo.childNodes[1].text.trim();
+                                sha1 = tooltipInfo.childNodes[4].text.trim();
+                            } catch {
+                                console.log(JSON.stringify(tooltipInfo));
+                                console.warn(`Error during fetching the sha1 and md5 for the forge "${href}". The result might be wrong.`);
+                            }
+                            const isSha1 = /\b([a-f0-9]{40})\b/i
+                            const isMd5 = /\b[a-f0-9]{32}\b/i;
+                            if (!isMd5.test(md5.trim())) {
+                                console.warn(`Illegal Md5 for "${href}": ${md5}`)
+                                md5 = "";
+                            }
+                            if (!isSha1.test(sha1.trim())) {
+                                console.warn(`Illegal Sha1 for "${href}": ${sha1}`)
+                                sha1 = "";
+                            }
                             return {
-                                name: url.childNodes[1].rawText.trim(),
-                                md5: tt.childNodes[1].text.trim(),
-                                sha1: tt.lastChild.text.trim(),
-                                path: url.attributes.href,
+                                name,
+                                md5,
+                                sha1,
+                                path: href,
                             };
                         });
                     const downloadVersionElem = e.querySelector(".download-version");
@@ -51,8 +82,14 @@ export namespace ForgeWebPage {
                     } else {
                         version = downloadVersionElem.text.trim();
                     }
-                    const installer = links.find((l) => l.name === "Installer");
-                    const universal = links.find((l) => l.name === "Universal");
+                    const installer = links.find((l) => l.name === "installer");
+                    const universal = links.find((l) => l.name === "universal");
+                    const changelog = links.find((l) => l.name === "changelog");
+                    const installerWin = links.find((l) => l.name === "installer-win");
+                    const source = links.find((l) => l.name === "source");
+                    const launcher = links.find((l) => l.name === "launcher");
+
+                    const mdk = links.find((l) => l.name === "mdk");
 
                     if (installer === undefined || universal === undefined) {
                         throw new Error("Cannot parse forge web since it missing installer and universal jar info.");
@@ -60,11 +97,13 @@ export namespace ForgeWebPage {
                     const result = {
                         version,
                         "date": e.querySelector(".download-time").text.trim(),
-                        "changelog": links[0],
+                        changelog,
                         installer,
-                        "installer-win": links.find((l) => l.name === "Installer-win"),
-                        "mdk": links.find((l) => l.name === "Mdk"),
+                        mdk,
                         universal,
+                        source,
+                        launcher,
+                        "installer-win": installerWin,
                         "mcversion": mcversion,
                         type,
                     };
@@ -153,10 +192,13 @@ export namespace ForgeWebPage {
         /**
          * The changelog info
          */
-        changelog: ForgeWebPage.Download;
+        changelog?: ForgeWebPage.Download;
         installer: ForgeWebPage.Download;
         mdk?: ForgeWebPage.Download;
         universal: ForgeWebPage.Download;
+        source?: ForgeWebPage.Download;
+        launcher?: ForgeWebPage.Download;
+
         /**
          * The type of the forge release. The `common` means the normal release.
          */
