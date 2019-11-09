f9ada4146d03e75a529db1790c89c76d3f6e4996
ci010 <cijhn@hotmail.com>
2019-11-09 17:19:41 +0800

    chore: clean the dir recursively

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
 
-const packages = fs.readdirSync("packages");
-for (const pack of packages) {
+function scandir(dir) {
     try {
-        for (const file of fs.readdirSync("packages/" + pack)) {
+        for (const file of fs.readdirSync(dir)) {
+            const f = `${dir}/${file}`;
             if (file.endsWith(".d.ts") || file.endsWith(".js") || file.endsWith(".js.map")) {
-                fs.unlinkSync(`packages/${pack}/${file}`);
+                fs.unlinkSync(f);
+            } else if (fs.statSync(f).isDirectory()) {
+                scandir(f);
             }
         }
-    } catch { }
+    } catch (e) {
+        console.error(e)
+    }
 }
+
+scandir("packages");
