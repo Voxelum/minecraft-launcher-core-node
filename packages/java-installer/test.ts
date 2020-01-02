f61788c7a07ecde44124719fc3dfacccde048708
ci010 <cijhn@hotmail.com>
2020-01-02 15:13:49 +0800

    test: adjust test mock

--- packages/java-installer/test.ts
+++ packages/java-installer/test.ts
@@ -2,11 +2,14 @@ jest.mock("child_process");
 
 import { exec } from "child_process";
 import { JavaInstaller } from "./index";
+import { promises } from "fs";
 
 describe("JavaInstaller", () => {
     const mockExc = exec as any as jest.Mock;
+    const stat = promises.stat;
     describe("#resolveJava", () => {
         test("should resolve old java version", async () => {
+            Reflect.set(promises, "stat", () => Promise.resolve());
             // VFS.setVirtualFS({ async exists() { return true } });
             mockExc.mockImplementationOnce((arg: string, cb: any) => {
                 cb(undefined, undefined, `java version "1.7.0_55"
@@ -15,8 +18,10 @@ describe("JavaInstaller", () => {
             });
             const inf = await JavaInstaller.resolveJava("path/to/java");
             expect(inf).toEqual({ path: "path/to/java", version: "1.7.0", majorVersion: 7 });
+            Reflect.set(promises, "stat", stat);
         });
         test("should resolve new java version", async () => {
+            Reflect.set(promises, "stat", () => Promise.resolve());
             // VFS.setVirtualFS({ async exists() { return true } });
             mockExc.mockImplementationOnce((arg: string, cb: any) => {
                 cb(undefined, undefined, `java 10.0.1 2018-04-17
@@ -25,27 +30,34 @@ describe("JavaInstaller", () => {
             });
             const inf = await JavaInstaller.resolveJava("path/to/java");
             expect(inf).toEqual({ path: "path/to/java", version: "10.0.1", majorVersion: 10 });
+            Reflect.set(promises, "stat", stat);
         });
         test("should return undefined if path not existed", async () => {
+            Reflect.set(promises, "stat", () => Promise.reject());
             // VFS.setVirtualFS({ async exists() { return false } });
             const inf = await JavaInstaller.resolveJava("path/to/java");
             expect(inf).toBeUndefined();
+            Reflect.set(promises, "stat", stat);
         });
         test("should return undefined if the process is not java", async () => {
+            Reflect.set(promises, "stat", () => Promise.resolve());
             // VFS.setVirtualFS({ async exists() { return true } });
             mockExc.mockImplementationOnce((arg: string, cb: any) => {
                 cb(undefined, undefined, "whatever");
             });
             const inf = await JavaInstaller.resolveJava("path/to/java");
             expect(inf).toBeUndefined();
+            Reflect.set(promises, "stat", stat);
         });
         test("should return undefined if the process is not java and return nothing in stderr", async () => {
+            Reflect.set(promises, "stat", () => Promise.resolve());
             // VFS.setVirtualFS({ async exists() { return true } });
             mockExc.mockImplementationOnce((arg: string, cb: any) => {
                 cb(undefined, undefined, undefined);
             });
             const inf = await JavaInstaller.resolveJava("path/to/java");
             expect(inf).toBeUndefined();
+            Reflect.set(promises, "stat", stat);
         });
-    })
+    });
 });
