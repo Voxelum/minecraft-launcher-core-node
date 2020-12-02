d72d4a92b2d1455f026c830472f810c3073aecf0
ci010 <cijhn@hotmail.com>
2020-12-02 14:13:25 +0800

    fix(mod-parser): don't guess fabric mod as forge mod
    don't guess fabric mod as forge mod unless it has forge class

--- packages/mod-parser/fabric.test.ts
+++ packages/mod-parser/fabric.test.ts
@@ -5,28 +5,30 @@ describe("Fabric", () => {
     const root = path.join(__dirname, "..", "..", "temp");
     const mockRoot = path.join(__dirname, "..", "..", "mock");
 
-    test("#readModMetadata", async () => {
-        const mod = await readModMetaData(path.join(mockRoot, "mods", "fabric-sample.jar"));
-        expect(mod).toBeTruthy();
-        expect(mod).toEqual({
-            schemaVersion: 1,
-            id: "appleskin",
-            version: "1.0.8",
-            name: "AppleSkin",
-            description: "Adds various food-related HUD improvements",
-            authors: ["squeek502"],
-            contact:
-            {
-                sources: "https://github.com/squeek502/AppleSkin",
-                homepage: "https://minecraft.curseforge.com/projects/appleskin",
-                issues: "https://github.com/squeek502/AppleSkin/issues"
-            },
-            license: "Unlicense",
-            icon: "assets/appleskin/appleskin.png",
-            environment: "*",
-            entrypoints: { client: ["squeek.appleskin.AppleSkin"] },
-            mixins: ["appleskin.mixins.json"],
-            depends: { fabricloader: ">=0.4.0", fabric: "*" }
+    describe("#readModMetadata", () => {
+        it("should read the simple fabric json", async () => {
+            const mod = await readModMetaData(path.join(mockRoot, "mods", "fabric-sample.jar"));
+            expect(mod).toBeTruthy();
+            expect(mod).toEqual({
+                schemaVersion: 1,
+                id: "appleskin",
+                version: "1.0.8",
+                name: "AppleSkin",
+                description: "Adds various food-related HUD improvements",
+                authors: ["squeek502"],
+                contact:
+                {
+                    sources: "https://github.com/squeek502/AppleSkin",
+                    homepage: "https://minecraft.curseforge.com/projects/appleskin",
+                    issues: "https://github.com/squeek502/AppleSkin/issues"
+                },
+                license: "Unlicense",
+                icon: "assets/appleskin/appleskin.png",
+                environment: "*",
+                entrypoints: { client: ["squeek.appleskin.AppleSkin"] },
+                mixins: ["appleskin.mixins.json"],
+                depends: { fabricloader: ">=0.4.0", fabric: "*" }
+            });
         });
-    });
+    })
 });
