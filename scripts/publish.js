2e9af74de30e2b10002dd2266f0aa5c36a495453
ci010 <cijhn@hotmail.com>
2020-12-20 14:37:09 +0800

    chore: correct the versioning

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,8 +3,7 @@ const { join } = require('path');
 const { execSync } = require('child_process');
 const { readFileSync } = require('fs');
 
-const tag = process.argv[process.argv.length - 1];
-console.log(`Publish to tag ${tag ?? 'latest'}`)
+console.log(`Publish to tag ${'latest'}`)
 
 for (const workspace of workspaces) {
     const cwd = join(__dirname, '..', workspace);
@@ -13,7 +12,7 @@ for (const workspace of workspaces) {
     const packageVersion = packageContent.version;
     const published = execSync(`npm show ${packageName} versions`);
     if (published.indexOf(packageVersion) === -1) {
-        const result = execSync(`npm publish --tag ${tag} --access public`, { cwd }).toString('utf-8');
+        const result = execSync(`npm publish --access public`, { cwd }).toString('utf-8');
         console.log(result);
     } else {
         console.log(`${packageName}@${packageVersion} is already published. Skip it.`)
