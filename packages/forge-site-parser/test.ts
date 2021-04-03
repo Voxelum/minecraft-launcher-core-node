48ae3216e58e71bc82f34afe9b0f76d2b31ad75c
dozono <chrislee.hao@gmail.com>
2021-04-03 18:18:11 +0800

    test(forge-site-parser): fix test compile

--- packages/forge-site-parser/test.ts
+++ packages/forge-site-parser/test.ts
@@ -14,21 +14,21 @@ describe("ForgeWebpage", () => {
                 expect(ver.type).toBeTruthy();
                 expect(ver.version).toBeTruthy();
 
-                expect(ver.installer.sha1).toBeTruthy();
-                expect(ver.installer.path).toBeTruthy();
-                expect(ver.installer.md5).toBeTruthy();
+                expect(ver.installer!.sha1).toBeTruthy();
+                expect(ver.installer!.path).toBeTruthy();
+                expect(ver.installer!.md5).toBeTruthy();
             }
             let ver = page.versions[page.versions.length - 2];
-            expect(ver.installer.md5).toEqual("2d24a32cce228d4cf3c42caf2e3cfe37");
+            expect(ver.installer!.md5).toEqual("2d24a32cce228d4cf3c42caf2e3cfe37");
             expect(ver.version).toEqual("28.0.4");
-            expect(ver.installer.sha1).toEqual("80ffade96232940422cbaf218ce6d424fd9192f2");
-            expect(ver.installer.path).toEqual("http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.14.4-28.0.4/forge-1.14.4-28.0.4-installer.jar");
+            expect(ver.installer!.sha1).toEqual("80ffade96232940422cbaf218ce6d424fd9192f2");
+            expect(ver.installer!.path).toEqual("http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.14.4-28.0.4/forge-1.14.4-28.0.4-installer.jar");
 
             ver = page.versions[page.versions.length - 1];
-            expect(ver.installer.md5).toEqual("325158d1f128d18e78decd624340f707");
-            expect(ver.installer.sha1).toEqual("5d52ef70aa57d00a1c75c36314cc4dff946f8320");
+            expect(ver.installer!.md5).toEqual("325158d1f128d18e78decd624340f707");
+            expect(ver.installer!.sha1).toEqual("5d52ef70aa57d00a1c75c36314cc4dff946f8320");
             expect(ver.version).toEqual("36.1.3");
-            expect(ver.installer.path).toEqual("/maven/net/minecraftforge/forge/1.16.5-36.1.3/forge-1.16.5-36.1.3-installer.jar");
+            expect(ver.installer!.path).toEqual("/maven/net/minecraftforge/forge/1.16.5-36.1.3/forge-1.16.5-36.1.3-installer.jar");
         });
         test("should be able to parse real forge", async () => {
 
