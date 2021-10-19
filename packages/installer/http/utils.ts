7dccc1d6fed006c52f4f42411ff748abb10b9ea6
ci010 <cijhn@hotmail.com>
2021-10-19 21:48:57 +0800

    fix(installer): support 308 redirect

--- packages/installer/http/utils.ts
+++ packages/installer/http/utils.ts
@@ -41,9 +41,9 @@ export function fetch(options: RequestOptions, agents: { http?: HttpAgent, https
       } else {
         let [req, agent] = options.protocol === "http:" ? [request, agents.http] : [requests, agents.https];
         let clientReq = req({ ...options, agent }, (m) => {
-          if (m.statusCode === 302 || m.statusCode === 301 || m.statusCode === 303) {
+          if ((m.statusCode === 302 || m.statusCode === 301 || m.statusCode === 303 || m.statusCode === 308) && typeof m.headers.location === 'string') {
             m.resume();
-            follow(mergeRequestOptions(options, urlToRequestOptions(new URL(m.headers.location!))));
+            follow(mergeRequestOptions(options, urlToRequestOptions(new URL(m.headers.location))));
           } else {
             m.url = m.url || format(options);
             clientReq.removeListener("error", reject);
