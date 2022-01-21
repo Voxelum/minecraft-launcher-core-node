53a4aa69a4adf4dc348987b577466e26923f9585
ci010 <cijhn@hotmail.com>
2022-01-21 23:56:08 +0800

    chore: skip empty dir

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 const { execSync } = require('child_process');
-const { readFileSync, readdirSync } = require('fs');
+const { readFileSync, readdirSync, existsSync } = require('fs');
 
 console.log(`Publish to tag ${'latest'}`)
 
@@ -8,6 +8,9 @@ const workspaces = readdirSync(join(__dirname, '../packages'))
 
 for (const workspace of workspaces) {
     const cwd = join(__dirname, '..', 'packages', workspace);
+    if (!existsSync(join(cwd, 'package.json'))) {
+        continue
+    }
     const packageContent = JSON.parse(readFileSync(join(cwd, 'package.json')).toString());
     const packageName = packageContent.name;
     const packageVersion = packageContent.version;
