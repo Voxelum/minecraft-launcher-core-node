d51215069c582b5b4d66f0672a671699c4f60c1c
CI010 <cijhn@hotmail.com>
2023-05-08 11:34:46 +0800

    fix: The validator is missed for validator refactor

--- packages/installer/zipValdiator.ts
+++ packages/installer/zipValdiator.ts
@@ -1,11 +1,10 @@
 import { ValidationError, Validator } from '@xmcl/file-transfer'
-import { FileHandle } from 'fs/promises'
 import { open } from '@xmcl/unzip'
 
 export class ZipValidator implements Validator {
-  async validate(fd: FileHandle, destination: string, url: string): Promise<void> {
+  async validate(destination: string, url: string): Promise<void> {
     try {
-      const file = await open(fd.fd, { autoClose: false, lazyEntries: true })
+      const file = await open(destination, { autoClose: false, lazyEntries: true })
       file.close()
     } catch (e) {
       throw new ValidationError('InvalidZipError', (e as any).message)
