13a0f749796ef540adc9235ce39f9bd3a9e01d70
ci010 <cijhn@hotmail.com>
2024-02-22 14:28:58 +0800

    feat: Support commom offline player algorithm

--- packages/user/offline.ts
+++ packages/user/offline.ts
@@ -1,4 +1,5 @@
-import { v3, v4 } from 'uuid'
+import { createHash } from 'crypto'
+import { v4 } from 'uuid'
 
 /**
  * Random generate a new token by uuid v4. It can be client or auth token.
@@ -8,13 +9,22 @@ export function newToken() {
   return v4().replace(/-/g, '')
 }
 
+export function getUUIDOfOfflinePlayer(username: string) {
+  const md5Bytes = createHash('md5').update(`OfflinePlayer:${username}`).digest()
+  md5Bytes[6] &= 0x0f /* clear version        */
+  md5Bytes[6] |= 0x30 /* set to version 3     */
+  md5Bytes[8] &= 0x3f /* clear variant        */
+  md5Bytes[8] |= 0x80 /* set to IETF variant  */
+  return md5Bytes.toString('hex')
+}
+
 /**
  * Create an offline auth. It'll ensure the user game profile's `uuid` is the same for the same `username`.
  *
  * @param username The username you want to have in-game.
  */
 export function offline(username: string, uuid?: string) {
-  const id = (uuid || v3(username, '00000000-0000-0000-0000-000000000000')).replace(/-/g, '')
+  const id = (uuid || getUUIDOfOfflinePlayer(username))
   const prof = {
     id,
     name: username,
