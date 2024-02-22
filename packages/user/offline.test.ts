53255fad6c6d1e1511616e6c8ba2cc849d73dfc6
ci010 <cijhn@hotmail.com>
2024-02-22 14:28:58 +0800

    feat: Support commom offline player algorithm
    MERGE TO: offline player
    
    MERGE TO: offline

--- packages/user/offline.test.ts
+++ packages/user/offline.test.ts
@@ -5,7 +5,7 @@ describe('#offline', () => {
   it('should be able to login ', () => {
     const offlineUser = offline('ci010')
     expect(offlineUser.selectedProfile.name).toEqual('ci010')
-    expect(offlineUser.selectedProfile.id).toEqual('942a4c3f681530e48e8ef00758a9128c')
+    expect(offlineUser.selectedProfile.id).toEqual('942a4c3f-6815-30e4-8e8e-f00758a9128c')
     expect(offlineUser.accessToken).toBeTruthy()
     expect(offlineUser.clientToken).toBeTruthy()
     expect(offlineUser.user!.id).toBeTruthy()
--- packages/user/offline.ts
+++ packages/user/offline.ts
@@ -1,4 +1,5 @@
-import { v3, v4 } from 'uuid'
+import { v4 } from 'uuid'
+import { getOfflineUUID } from 'user-offline-uuid'
 
 /**
  * Random generate a new token by uuid v4. It can be client or auth token.
@@ -8,13 +9,15 @@ export function newToken() {
   return v4().replace(/-/g, '')
 }
 
+export { getOfflineUUID }
+
 /**
  * Create an offline auth. It'll ensure the user game profile's `uuid` is the same for the same `username`.
  *
  * @param username The username you want to have in-game.
  */
 export function offline(username: string, uuid?: string) {
-  const id = (uuid || v3(username, '00000000-0000-0000-0000-000000000000')).replace(/-/g, '')
+  const id = (uuid || getOfflineUUID(username))
   const prof = {
     id,
     name: username,
