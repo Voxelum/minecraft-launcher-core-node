a3a26c840d1e26ce0ec7749fdc8b41bc25fd01f0
ci010 <cijhn@hotmail.com>
2024-02-25 18:12:50 +0800

    BREAKING CHANGE: new download impl

--- packages/file-transfer/agent.ts
+++ packages/file-transfer/agent.ts
@@ -1,179 +1,57 @@
-import { FileHandle } from 'fs/promises'
-import CachePolicy from 'http-cache-semantics'
-import { Dispatcher, errors, getGlobalDispatcher } from 'undici'
-import { AbortSignal } from './abort'
-import { CheckpointHandler, createInMemoryCheckpointHandler } from './checkpoint'
-import { DownloadAbortError } from './error'
-import { parseRangeInfo } from './metadata'
-import { createDefaultRetryHandler, RetryPolicy } from './retry'
-import { head, range } from './range'
-import { DefaultRangePolicy, Range, RangePolicy } from './rangePolicy'
-import { ProgressController } from './progress'
+import { Agent, Dispatcher, RedirectHandler, RetryHandler } from 'undici'
 
-const { ResponseStatusCodeError, RequestAbortedError } = errors
-export interface DownloadAgentOptions {
-  retryHandler?: RetryPolicy
-  rangePolicy?: RangePolicy
-  dispatcher?: Dispatcher
-  checkpointHandler?: CheckpointHandler
-}
-
-export function resolveAgent(agent?: DownloadAgent | DownloadAgentOptions) {
-  return agent instanceof DownloadAgent
-    ? agent
-    : new DownloadAgent(
-      agent?.retryHandler ?? createDefaultRetryHandler(3),
-      agent?.rangePolicy ?? new DefaultRangePolicy(2 * 1024 * 1024, 4),
-      agent?.dispatcher ?? getGlobalDispatcher(),
-      agent?.checkpointHandler ?? createInMemoryCheckpointHandler(),
-    )
-}
-
-export class DownloadAgent {
-  constructor(
-    readonly retryHandler: RetryPolicy,
-    readonly rangePolicy: RangePolicy,
-    readonly dispatcher: Dispatcher,
-    readonly checkpointHandler: CheckpointHandler | undefined,
-  ) {
-
+class NeoRedirectHandler extends RedirectHandler {
+  constructor(dispatch: Dispatcher['dispatch'], maxRedirections: number, opts: Dispatcher.RequestOptions, handler: Dispatcher.DispatchHandlers, follow: boolean) {
+    super(dispatch as any, maxRedirections, opts, handler, follow)
   }
 
-  private async head(url: URL, headers: Record<string, string>, signal?: AbortSignal) {
-    const kernel = head(url, headers, signal, this.dispatcher)
-
-    let current = await kernel.next()
-    let attempt = 0
-    for (; !current.done; current = await kernel.next(), attempt++) {
-      const error = current.value
-      if (error instanceof RequestAbortedError || !await this.retryHandler.retry(url, attempt, error)) {
-        // won't retry anymore
-        await kernel.throw(error)
-      }
-    }
-
-    return current.value
+  onConnect(abort: () => void) {
+    // @ts-ignore
+    this.abort = abort
+    // @ts-ignore
+    this.handler.onConnect(abort, { history: this.history, opts: this.opts })
   }
+}
 
-  async dispatch(url: URL, method: string, headers: Record<string, string>, destination: string, handle: FileHandle, progressController: ProgressController | undefined, abortSignal: AbortSignal | undefined) {
-    let targetUrl: URL = url
-    let ranges: Range[] | undefined
+export function getDefaultAgentOptions(retry?: RetryHandler.RetryOptions, defaultMaxRedirections = 5) {
+  const options = {
+    connections: 16,
+    interceptors: {
+      Agent: [
+        createRedirectInterceptor(defaultMaxRedirections),
+      ],
+      Client: [
+        createRetryInterceptor(retry),
+        createRedirectInterceptor(defaultMaxRedirections),
+      ],
+    },
+  }
+  return options
+}
 
-    const req = {
-      url: url.toString(),
-      method,
-      headers,
+export function createRetryInterceptor(retry?: RetryHandler.RetryOptions): Dispatcher.DispatchInterceptor {
+  return (dispatch) => {
+    return function Intercept(opts, handler) {
+      return dispatch(opts, new RetryHandler({ ...opts, retryOptions: retry }, {
+        dispatch,
+        handler,
+      }))
     }
-    const checkpoint = await this.checkpointHandler?.lookup(url, handle, destination)
-    let policy = checkpoint?.policy
-    if (checkpoint) {
-      if (checkpoint.policy.satisfiesWithoutRevalidation(req)) {
-        // Use checkpoint without revalidate
-        ranges = checkpoint.ranges
-        targetUrl = new URL(checkpoint.url)
-      } else {
-        // try to revalidate
-        headers = checkpoint.policy.revalidationHeaders(req) as Record<string, string>
-      }
-    }
+  }
+}
 
-    let total = -1
+export function createRedirectInterceptor(defaultMaxRedirections: number): Dispatcher.DispatchInterceptor {
+  return (dispatch) => {
+    return function Intercept(opts, handler) {
+      const { maxRedirections = defaultMaxRedirections } = opts as any
 
-    if (!ranges) {
-      let location = url.toString()
-      // Fetch download metadata
-      const response = await this.head(url, headers, abortSignal)
-
-      const history = (response.context as any)?.history as (URL[] | undefined)
-      location = history?.[history?.length - 1].toString() || location
-
-      // Try to revalidate the resource
-      const revalidation = policy?.revalidatedPolicy(req, {
-        headers: response.headers,
-        status: response.statusCode,
-      })
-
-      if (!revalidation?.modified && checkpoint) {
-        // Resource is not modified and we trust the last checkpoint
-        ranges = checkpoint.ranges
-        targetUrl = new URL(location)
-        total = checkpoint.contentLength
-      } else if (response.statusCode === 200 || response.statusCode === 201) {
-        // Respect head request result
-        // Calculate the resource slices/ranges
-        const { contentLength, isAcceptRanges } = parseRangeInfo(response.headers)
-        ranges = contentLength && isAcceptRanges
-          ? this.rangePolicy.computeRanges(contentLength)
-          : [{ start: 0, end: contentLength - 1 }]
-        targetUrl = new URL(location)
-        total = contentLength
-        await handle.truncate(total)
-      } else if (response.statusCode === 405) {
-        // Do not support HEAD request, we just download without range
-        ranges = [{ start: 0, end: -1 }]
-        await handle.truncate()
-      } else {
-        // @ts-ignore
-        throw new ResponseStatusCodeError(`Fail to check metadata of ${url}`, response.statusCode, response.headers, response.body)
+      if (!maxRedirections) {
+        return dispatch(opts, handler)
       }
 
-      // Update the checkpoint policy
-      policy = revalidation?.policy ?? new CachePolicy(req, {
-        status: response.statusCode,
-        headers: response.headers,
-      })
-
-      this.checkpointHandler?.put(url, handle, destination, {
-        policy,
-        ranges,
-        contentLength: total,
-        url: url.toString(),
-      })
+      const redirectHandler = new NeoRedirectHandler(dispatch, maxRedirections, opts, handler, false)
+      opts = { ...opts, maxRedirections: 0 } as any // Stop sub dispatcher from also redirecting.
+      return dispatch(opts, redirectHandler)
     }
-
-    // Update the download status
-    const remaining = ranges.map((s) => s.end - s.start).reduce((a, b) => a + b, 0)
-    progressController?.onProgress(targetUrl, -1, total - remaining, total)
-    const _ranges = ranges
-    const results = await Promise.all(ranges.map(async (r) => {
-      // Start download range
-      const kernel = range(targetUrl, r, headers, handle, total, progressController, abortSignal, this.dispatcher)
-
-      let attempt = 0
-      for (let current = await kernel.next(); !current.done; current = await kernel.next(), attempt++) {
-        // The yield value must be error
-        const err = current.value
-        if (err instanceof RequestAbortedError || !await this.retryHandler.retry(url, attempt, err)) {
-          // won't retry anymore
-          await kernel.return(err)
-          return err
-        }
-
-        const remaining = _ranges.map((s) => s.end - s.start).reduce((a, b) => a + b, 0)
-        progressController?.onProgress(targetUrl, -1, total - remaining, total)
-      }
-    }))
-
-    const errors = results.filter((r) => !!r)
-
-    if (errors[0] instanceof RequestAbortedError) {
-      // Throw abort error anyway
-      throw new DownloadAbortError(`Download is aborted by user: ${targetUrl}`, [], headers, destination, ranges)
-    }
-
-    if (errors.length > 0) {
-      if (policy && ranges.length > 0) {
-        await this.checkpointHandler?.put(url, handle, destination, {
-          url: targetUrl.toString(),
-          policy,
-          contentLength: total,
-          ranges,
-        }).catch(() => { })
-      }
-
-      throw errors[0]
-    }
-
-    await this.checkpointHandler?.delete(url, handle, destination)
   }
 }
--- packages/file-transfer/download.ts
+++ packages/file-transfer/download.ts
@@ -1,13 +1,16 @@
-import { FileHandle, mkdir, open, rename, stat, unlink } from 'fs/promises'
-import { constants, existsSync, unlinkSync } from 'fs'
+import { createWriteStream } from 'fs'
+import { mkdir, rename, unlink } from 'fs/promises'
 import { dirname } from 'path'
-import { errors } from 'undici'
-import { DownloadAgent, resolveAgent } from './agent'
-import { DownloadAbortError, DownloadAggregateError, DownloadFileSystemError } from './error'
+import { PassThrough, Writable } from 'stream'
+import { Agent, Dispatcher, errors, stream } from 'undici'
+// @ts-ignore
+import { parseRangeHeader } from 'undici/lib/core/util'
 import { AbortSignal } from './abort'
-import { resolveProgressController, ProgressController } from './progress'
-import { ChecksumValidatorOptions, resolveValidator, ValidationError, Validator } from './validator'
-import assert from 'assert'
+import { getDefaultAgentOptions } from './agent'
+import { CheckpointHandler } from './checkpoint'
+import { ProgressController, resolveProgressController } from './progress'
+import { DefaultRangePolicy, Range, RangePolicy } from './rangePolicy'
+import { ChecksumValidatorOptions, Validator, resolveValidator } from './validator'
 
 export interface DownloadBaseOptions {
   /**
@@ -15,18 +18,22 @@ export interface DownloadBaseOptions {
    */
   headers?: Record<string, any>
   /**
-   * The download agent
+   * The range policy to compute the ranges to download
    */
-  agent?: DownloadAgent
+  rangePolicy?: RangePolicy
   /**
-   * Re-validate the file after download success
-   * @default false
+   * The undici dispatcher
    */
+  dispatcher?: Dispatcher
+  /**
+   * The checkpoint handler to save & restore the download progress
+   */
+  checkpointHandler?: CheckpointHandler
+
+  skipHead?: boolean
+
   skipRevalidate?: boolean
-  /**
-   * Should skip prevalidate the file
-   * @default false
-   */
+
   skipPrevalidate?: boolean
 }
 
@@ -61,27 +68,121 @@ export interface DownloadOptions extends DownloadBaseOptions {
   pendingFile?: string
 }
 
-async function openFile(destination: string, validator: Validator, url: string, skipPrevalidate: boolean) {
-  let fd = undefined as FileHandle | undefined
-  try {
-    // use O_RDWR for read write which won't be truncated
-    fd = await open(destination, constants.O_RDWR | constants.O_CREAT)
+interface Context {
+  history: URL[]
+  opts: Dispatcher.DispatchOptions
+}
+interface Metadata {
+  origin?: string
+  pathname?: string
+  offset: number
+  total: number
+  acceptRange: boolean
+}
 
-    const size = (await fd.stat()).size
-    // pre-validate the file
-    if (size !== 0 && !skipPrevalidate) {
-      const error = await validator.validate(destination, url).catch((e) => e)
-      // if the file size is not 0 and checksum matched, we just don't process the file
-      if (!error) {
-        await fd?.close().catch(() => { })
+async function head(url: string, headers: Record<string, string>, dispatcher: Dispatcher, signal?: AbortSignal) {
+  try {
+    const { opaque } = await stream(url, {
+      method: 'HEAD',
+      headers,
+      dispatcher,
+      signal,
+      opaque: {},
+      headersTimeout: 5000,
+      bodyTimeout: 5000,
+      throwOnError: true,
+    }, ({ opaque, headers, context }) => {
+      const length = headers['content-length'] ? parseInt(headers['content-length'] as string) : 0
+      const rangeHeader = parseRangeHeader(headers['content-range'])
+      const offset = rangeHeader?.start ?? 0
+      const total = rangeHeader?.size || length
+      const ctx = context as Context
+      const metadata = opaque as Metadata
+
+      metadata.offset = offset
+      metadata.total = total
+      metadata.acceptRange = headers['accept-ranges'] === 'bytes'
+
+      if (ctx) {
+        metadata.pathname = ctx.opts.path
+        metadata.origin = ctx.opts.origin as string
+      }
+
+      return new PassThrough()
+    })
+
+    return opaque as Metadata
+  } catch (e) {
+    if (e instanceof errors.ResponseStatusCodeError) {
+      if (e.statusCode === 405) {
         return undefined
       }
     }
-    return fd
-  } catch (e) {
-    await fd?.close().catch(() => { })
     throw e
   }
+}
+
+async function get(url: string, destination: string, headers: Record<string, string>, range: Range | undefined,
+  dispatcher: Dispatcher, progress: (url: URL, chunkSize: number, written: number, partLength: number, totalLength: number) => void, signal?: AbortSignal) {
+  const parsedUrl = new URL(url)
+
+  let writable: Writable | undefined
+
+  try {
+    const requestHeader = { ...headers }
+    if (range) {
+      requestHeader.range = `bytes=${range.start}-${range.end}`
+    }
+    await stream(url, {
+      method: 'GET',
+      maxRedirections: 5,
+      headers: requestHeader,
+      throwOnError: true,
+      dispatcher,
+      signal,
+    }, ({ statusCode, headers }) => {
+      if (statusCode >= 300) {
+        // unreachable
+        return new PassThrough()
+      }
+
+      const length = headers['content-length'] ? parseInt(headers['content-length'] as string) : 0
+      const rangeHeader = parseRangeHeader(headers['content-range'])
+      const offset = rangeHeader?.start ?? range?.start
+      const totalLength = rangeHeader?.size ?? 0
+      let written = 0
+
+      writable = createWriteStream(destination, {
+        start: offset,
+        highWaterMark: 1024 * 1024,
+      })
+      const write = writable.write.bind(writable)
+      writable.write = (chunk, ...rest) => {
+        written += chunk.length
+        progress(parsedUrl, chunk.length, written, length, totalLength)
+        return write(chunk, ...rest as any)
+      }
+      progress(parsedUrl, 0, written, length, totalLength)
+
+      return writable
+    })
+  } catch (e) {
+    return e
+  }
+}
+
+function computeRanges(metadata: Metadata | undefined, rangePolicy: RangePolicy) {
+  if (!metadata) return [undefined]
+  const ranges = rangePolicy.computeRanges(metadata.total)
+  if (ranges.length > 1) return ranges
+  return [undefined]
+}
+
+function getUrl(metadata: Metadata | undefined, original: string) {
+  if (!metadata || !metadata.pathname || !metadata.origin) return original
+  const url = new URL(metadata.origin)
+  url.pathname = metadata.pathname
+  return url.toString()
 }
 
 /**
@@ -94,109 +195,102 @@ export async function download(options: DownloadOptions) {
   const progressController = resolveProgressController(options.progressController)
   const validator = resolveValidator(options.validator)
   const abortSignal = options.abortSignal
-  const agent = resolveAgent(options.agent)
-  const skipRevalidate = options.skipRevalidate ?? false
-  const skipPrevalidate = options.skipPrevalidate ?? false
   const pendingFile = options.pendingFile
-
-  let fd = undefined as FileHandle | undefined
+  const skipPrevalidate = options.skipPrevalidate
+  const rangePolicy = options?.rangePolicy ?? new DefaultRangePolicy(2 * 1024 * 1024, 4)
+  const dispatcher = options?.dispatcher ?? new Agent(getDefaultAgentOptions())
 
   await mkdir(dirname(destination), { recursive: true }).catch(() => { })
-  // Access file handle
-  try {
+
+  if (!skipPrevalidate) {
+    const error = await validator.validate(destination, urls[0]).catch((e) => e)
+    if (!error) {
+      // file is already downloaded and validated
+      return
+    }
+
     if (pendingFile) {
       const error = await validator.validate(destination, urls[0]).catch((e) => e)
       if (!error) {
         // file is already downloaded and validated
-        return
-      }
-      fd = await openFile(pendingFile, validator, urls[0], skipPrevalidate)
-      if (!fd) {
-        // file is already downloaded and validated
         // we will overwrite destination with pending file
-        await unlink(destination).catch((e) => undefined)
+        await unlink(destination).catch(() => undefined)
         await rename(pendingFile, destination)
         return
       }
-    } else {
-      fd = await openFile(destination, validator, urls[0], skipPrevalidate)
-      if (!fd) {
-        // file is already downloaded and validated
-        return
-      }
     }
-  } catch (e) {
-    throw new DownloadFileSystemError(`Fail to get access on ${destination}`, urls, headers, destination, e)
   }
 
-  // Start to download
+  const aggregate: Error[] = []
+  for (const url of urls) {
+    const decorate = (e: any, phase: string) => Object.assign(e, {
+      phase,
+      urls,
+      url,
+      headers,
+      destination,
+    })
+    const metadataOrMetadata = !options.skipHead
+      ? await head(url, headers, dispatcher, abortSignal).catch((e) => {
+        return decorate(e, 'head') as Error
+      })
+      : undefined
+    if (metadataOrMetadata instanceof Error) {
+      if (metadataOrMetadata instanceof errors.RequestAbortedError) throw metadataOrMetadata
+      aggregate.push(metadataOrMetadata)
+      continue
+    }
+    const metadata = metadataOrMetadata
+    const ranges = metadata?.acceptRange ? computeRanges(metadata, rangePolicy) : [undefined]
+    const redirectedUrl = getUrl(metadata, url)
+    const writtens = ranges.map(() => 0)
+    const totals = ranges.map(() => 0)
+    const results = await Promise.all(ranges.map(
+      (range, index) => get(redirectedUrl, destination, headers, range, dispatcher, (url, chunk, written, partLength, totalLength) => {
+        writtens[index] = written
+        totals[index] = partLength
+        const writtenTotal = writtens.reduce((a, b) => a + b, 0)
+        const totalTotal = metadata?.total || totalLength || totals.reduce((a, b) => a + b, 0)
+        progressController.onProgress(url, chunk, writtenTotal, totalTotal)
+      }, abortSignal)),
+    )
 
-  try {
-    // File descriptor scope
-    try {
-      // Aggregated errors
-      let aggregatedErrors: { url: string }[] = []
-      for (const url of urls) {
+    let noErrors = true
+    for (const e of results) {
+      if (!e) continue
+      if (e instanceof errors.RequestAbortedError) throw e
+      noErrors = false
+      aggregate.push(decorate(e, 'get'))
+    }
+
+    if (noErrors) {
+      if (pendingFile) {
+        await unlink(destination).catch(() => undefined)
         try {
-          await agent.dispatch(new URL(url), 'GET', headers, destination, fd, progressController, abortSignal)
-          await fd.datasync()
-          if (!skipRevalidate) {
-            await validator.validate(pendingFile || destination, url)
-          }
-          // Dismiss all errors
-          aggregatedErrors = []
-          break
+          await rename(pendingFile, destination)
         } catch (e) {
-          if (e instanceof errors.RequestAbortedError) {
-            // User abortion during HEAD
-            throw new DownloadAbortError('Download is aborted by user.', urls, headers, destination, [])
-          }
-          if (e instanceof DownloadAbortError) {
-            // User abortion should throw anyway
-            throw e
-          }
-
-          aggregatedErrors.push(Object.assign((e as any), {
-            url,
-          }))
+          throw decorate(e, 'rename')
         }
       }
-
-      if (aggregatedErrors.length > 0) {
-        throw aggregatedErrors
-      }
-
-      // No error, so we are done
-      if (pendingFile) {
-        // If pending file, we need to rename it to destination with overwrite
-        await unlink(destination).catch(() => undefined)
-        await rename(pendingFile, destination).catch((e) => {
-          throw new DownloadFileSystemError('Download file destination already existed', urls, headers, destination, e)
-        })
-      }
-    } finally {
-      await fd.close().catch(() => { })
+      return
     }
-  } catch (e) {
-    if (e instanceof DownloadAbortError) {
-      e.urls = urls
-      throw e
-    }
-
-    if (e instanceof DownloadFileSystemError) {
-      throw e
-    }
-
-    assert(e instanceof Array)
-
-    if (e[e.length - 1] instanceof ValidationError) {
-      // Last download corrupted...
-      await unlink(destination).catch(() => { })
-      if (pendingFile) {
-        await unlink(pendingFile).catch(() => { })
-      }
-    }
-
-    throw new DownloadAggregateError(e, 'Multiple errors occurred during download process', urls, headers, destination)
   }
+
+  if (aggregate.length > 1) {
+    const flatten = [] as Error[]
+    const flatError = (e: any) => {
+      if (e instanceof AggregateError) {
+        for (const err of e.errors) {
+          flatError(err)
+        }
+      }
+      flatten.push(e)
+    }
+    for (const e of aggregate) {
+      flatError(e)
+    }
+    throw new AggregateError(flatten)
+  }
+
+  throw aggregate[0]
 }
