5b320aa92b9c7576b88786d2fb39c8241f0ea7b5
ci010 <cijhn@hotmail.com>
2025-12-10 23:24:05 +0800

    refactor: Add new package

---
+++ packages/resource/ResourceType.ts
@@ -0,0 +1,10 @@
+
+export enum ResourceType {
+  Forge = 'forge',
+  Neoforge = 'neoforge',
+  Liteloader = 'liteloader',
+  Fabric = 'fabric',
+  Quilt = 'quilt',
+  ResourcePack = 'resourcepack',
+  ShaderPack = 'shaderpack'
+}
